7cfd69cc3d3a8445cd23319a780d7c51ff723e7a
Asia <dobrowolska.krakow@gmail.com>
2024-03-15 17:35:28 +0100

    .env corrections

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/charity-run/' : '/',
   plugins: [react()],
 });
