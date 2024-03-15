6b6a48d98809dc4aa05dd5e677f5a36a77366a9a
Asia <dobrowolska.krakow@gmail.com>
2024-03-15 17:41:05 +0100

    GH-pages configuration

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
+const basename =
+  window.location.hostname === 'astrix1234.github.io' ? '/charity-run' : '';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/charity-run">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
