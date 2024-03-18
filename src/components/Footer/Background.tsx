93eaf8288c1804a5c0e4190eb924da596d08861f
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-18 08:41:57 +0100

    Add Background component

---
+++ src/components/Footer/Background.tsx
@@ -0,0 +1,29 @@
+export const Background = () => {
+  return (
+    <svg
+      width="1400"
+      height="800"
+      viewBox="0 0 1400 800"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 68.9002C0 68.9002 112.5 0 345 0C646 -0.00102728 713 128.337 1021.5 128.337C1228.5 128.337 1440 68.9002 1440 68.9002V800H0V68.9002Z"
+        fill="url(#paint0_linear_7_2702)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_7_2702"
+          x1="752"
+          y1="55.682"
+          x2="1068.26"
+          y2="932.64"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#20A5AA" />
+          <stop offset="0.927158" stop-color="white" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
