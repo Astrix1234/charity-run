fae85f086e86245e3dd8caafda888da6ce085f5e
Asia <dobrowolska.krakow@gmail.com>
2024-03-18 13:10:33 +0100

    Routes and Pages

---
+++ src/pages/OurAmbassadorsPage/OurAmbassadorsPage.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async';
+
+export default function OurAmbassadorsPage() {
+  return (
+    <div>
+      <Helmet>
+        <title>Our Ambassadors</title>
+      </Helmet>
+      <h1>Our Ambassadors</h1>
+    </div>
+  );
+}
