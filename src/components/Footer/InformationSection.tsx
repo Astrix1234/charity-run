f7e235844ab55d54bdbb85351ac7ddc0f2a910a6
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-20 10:30:23 +0100

    Move all text information to separate file

--- src/components/Footer/InformationSection.tsx
+++ src/components/Footer/InformationSection.tsx
@@ -1,32 +1,11 @@
 import styled from 'styled-components';
 import { Information } from './Information';
+import { contactParagraphs, supportParagraphs, usefulLinks } from './text';
 
 const StyledSection = styled.div`
   display: flex;
   gap: 4rem;
 `;
-
-const contactParagraphs = [
-  'Adres',
-  '93-513 Łódź',
-  'ul. Pabianicka 62',
-  'E-mail:',
-];
-
-const supportParagraphs = [
-  'Konto bankowe',
-  'PKO BP:',
-  '31 1020 3352 0000 1202 0241 5990',
-  'Dopisek: "Darowizna na cele statutowe"',
-  'KRS: 0000069531',
-  'NIP: 729-23-25-734',
-];
-
-const usefulLinks = {
-  'O fundacji': '#',
-  'Strefa uczestnika': '#',
-  Wolontariat: '#',
-};
 
 export const InformationSection = () => {
   return (
---
+++ src/components/Footer/text.tsx
@@ -0,0 +1,21 @@
+export const contactParagraphs = [
+  'Adres',
+  '93-513 Łódź',
+  'ul. Pabianicka 62',
+  'E-mail:',
+];
+
+export const supportParagraphs = [
+  'Konto bankowe',
+  'PKO BP:',
+  '31 1020 3352 0000 1202 0241 5990',
+  'Dopisek: "Darowizna na cele statutowe"',
+  'KRS: 0000069531',
+  'NIP: 729-23-25-734',
+];
+
+export const usefulLinks = {
+  'O fundacji': '#',
+  'Strefa uczestnika': '#',
+  Wolontariat: '#',
+};
