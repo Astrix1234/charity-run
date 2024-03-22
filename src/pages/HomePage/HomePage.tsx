18aebeb55ebb4ffe1d3f4f387d6e323555622641
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2024-03-22 22:09:20 +0100

    created HematoRunWhyIsItWorth component and updated vars

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import { HematoRunInfo } from '../../components/HematoRunInfo/HematoRunInfo';
 import { HematoRunMainTitle } from '../../components/HematoRunMainTitle/HematoRunMainTitle';
+import { HematoRunWhyIsItWorth } from '../../components/HematoRunWhyIsItWorth/HematoRunWhyIsItWorth';
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       </Helmet>
       <HematoRunMainTitle />
       <HematoRunInfo />
+      <HematoRunWhyIsItWorth />
     </div>
   );
 }
