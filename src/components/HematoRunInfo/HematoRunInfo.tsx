33e3ff0becab0c52c8ab42821f385229c8ad0244
Asia <dobrowolska.krakow@gmail.com>
2024-03-23 18:59:25 +0100

    Info about the run

--- src/components/HematoRunInfo/HematoRunInfo.tsx
+++ src/components/HematoRunInfo/HematoRunInfo.tsx
@@ -1,9 +1,66 @@
 import scss from './HematoRunInfo.module.scss';
+import { useState } from 'react';
+import { useLanguageStore } from '../../Zustand/useLanguageStore';
+import translations from './translations';
 
 export const HematoRunInfo = () => {
+  const [rolledUp, setRolledUp] = useState(true);
+  const { language } = useLanguageStore();
+  const t = translations[language];
+
+  const handleClick = () => {
+    setRolledUp(!rolledUp);
+  };
+
   return (
-    <section>
-      <h1 className={scss.title}></h1>
+    <section className={scss.runInfo}>
+      <div className={scss.runInfo__container}>
+        <div className={scss.runInfo__titleContainer}>
+          <p className={scss.runInfo__comment}>//{t.charityRun}</p>
+          <h2 className={scss.runInfo__title}>{t.hematoRun}</h2>
+          <div className={scss.runInfo__underlines}></div>
+        </div>
+        <div
+          className={`${scss.runInfo__text} ${
+            !rolledUp ? scss.runInfo__developed : ''
+          } `}
+        >
+          <p className={scss.runInfo__paragraph}>{t.runInfo1}</p>
+          <p className={scss.runInfo__paragraph}>
+            <b>{t.runInfo2}</b> {t.runInfo3}
+          </p>
+          <p className={scss.runInfo__paragraph}>{t.runInfo4}</p>
+          <p className={scss.runInfo__paragraph}>
+            {t.runInfo5} <b>{t.runInfo6}</b>
+          </p>
+          <p className={scss.runInfo__paragraph}>
+            {t.runInfo7} <b>{t.runInfo8}</b>&nbsp;{t.runInfo9}
+          </p>
+          <p className={scss.runInfo__paragraph}>
+            {t.runInfo10} <b>{t.runInfo11}</b>&nbsp;{t.runInfo12}
+          </p>
+          <p className={scss.runInfo__paragraph}>{t.runInfo13}</p>
+          <p className={scss.runInfo__paragraph}>{t.runInfo14}</p>
+          {rolledUp ? (
+            <button
+              className={scss.runInfo__button}
+              type="button"
+              onClick={handleClick}
+            >
+              {t.moreAbout}
+            </button>
+          ) : (
+            <button
+              className={scss.runInfo__button}
+              type="button"
+              onClick={handleClick}
+            >
+              {t.scrollDown}
+            </button>
+          )}
+        </div>
+        <div className={scss.runInfo__runningHelp}></div>
+      </div>
     </section>
   );
 };
