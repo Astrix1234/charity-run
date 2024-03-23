ed649c79c5537d3c3838900d7fb1dedbdbceb44e
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2024-03-23 19:37:08 +0100

    added all styles to why is it worth section

--- src/components/HematoRunWhyIsItWorth/HematoRunWhyIsItWorth.tsx
+++ src/components/HematoRunWhyIsItWorth/HematoRunWhyIsItWorth.tsx
@@ -2,8 +2,13 @@ import scss from './HematoRunWhyIsItWorth.module.scss';
 import { Button } from '../Button/Button';
 import { useLanguageStore } from '../../Zustand/useLanguageStore';
 import translations from './translations';
-import { Link } from 'react-router-dom';
 import { IconBloodCell } from '../../Icons/IconBloodCell/IconBloodCell';
+import { IconNumberOne } from '../../Icons/IconNumbers/IconNumberOne/IconNumberOne';
+import { IconNumberTwo } from '../../Icons/IconNumbers/IconNumberTwo/IconNumberTwo';
+import { IconNumberThree } from '../../Icons/IconNumbers/IconNumberThree/IconNumberThree';
+import { IconNumberFour } from '../../Icons/IconNumbers/IconNumberFour/IconNumberFour';
+import { IconNumberFive } from '../../Icons/IconNumbers/IconNumberFive/IconNumberFive';
+import { IconWavyArrowDown } from '../../Icons/IconWavyArrowDown/IconWavyArrowDown';
 
 export const HematoRunWhyIsItWorth = () => {
   const { language } = useLanguageStore();
@@ -16,9 +21,13 @@ export const HematoRunWhyIsItWorth = () => {
   return (
     <section className={scss.whyIsItWorth}>
       <div className={scss.whyIsItWorth__container}>
+        <div className={scss.whyIsItWorth__IconBg}>
+          {' '}
+          <IconWavyArrowDown />
+        </div>
         <div className={scss.whyIsItWorth__titleContainer}>
           <h2 className={scss.whyIsItWorth__title}>{t.title}</h2>
-          <div className={scss.whyIsItWorth__icon}>
+          <div className={scss.whyIsItWorth__iconBlood}>
             <IconBloodCell />
           </div>
         </div>
@@ -28,26 +37,43 @@ export const HematoRunWhyIsItWorth = () => {
               className={`${scss['whyIsItWorth__item']} ${scss['whyIsItWorth__item-fundraising']}`}
             >
               <p>{t.fundraising}</p>
+              <div>
+                <IconNumberOne />
+              </div>
             </li>
             <li
               className={`${scss['whyIsItWorth__item']} ${scss['whyIsItWorth__item-awareness']}`}
             >
+              <div>
+                <IconNumberTwo />{' '}
+              </div>
               <p>{t.awareness}</p>
             </li>
             <li
               className={`${scss['whyIsItWorth__item']} ${scss['whyIsItWorth__item-opportunity']}`}
             >
               <p>{t.opportunity}</p>
+              <div>
+                {' '}
+                <IconNumberThree />
+              </div>
             </li>
             <li
               className={`${scss['whyIsItWorth__item']} ${scss['whyIsItWorth__item-joy']}`}
             >
+              <div>
+                {' '}
+                <IconNumberFour />
+              </div>{' '}
               <p>{t.joy}</p>
             </li>
             <li
               className={`${scss['whyIsItWorth__item']} ${scss['whyIsItWorth__item-family']}`}
             >
               <p>{t.family}</p>
+              <div>
+                <IconNumberFive />
+              </div>
             </li>
           </ul>
         </div>
