043961f140c16ff9bb121947ac55c7315ffe93b6
Asia <dobrowolska.krakow@gmail.com>
2024-03-24 20:37:22 +0100

    corrections of conflicts

--- src/Icons/IconPlus/IconPlus.tsx
+++ src/Icons/IconPlus/IconPlus.tsx
@@ -12,14 +12,14 @@ export const IconPlus: React.FC<IconPlusProps> = ({ onClick }) => {
       viewBox="0 0 18 18"
       fill="none"
     >
-      <line y1="8.5" x2="18" y2="8.5" stroke="#2E2530" stroke-width="3" />
+      <line y1="8.5" x2="18" y2="8.5" stroke="#2E2530" strokeWidth="3" />
       <line
         x1="9.5"
         y1="6.55671e-08"
         x2="9.5"
         y2="18"
         stroke="#2E2530"
-        stroke-width="3"
+        strokeWidth="3"
       />
     </svg>
   );
