3b63f0b199e3472add0129bd263edfd2281eb3e6
PiotrDrechsler <pdrech@gmail.com>
2024-03-25 12:59:50 +0100

    translation

--- src/components/HematoRunRoute/HematoRunRoute.tsx
+++ src/components/HematoRunRoute/HematoRunRoute.tsx
@@ -1,15 +1,20 @@
+import { useLanguageStore } from '../../Zustand/useLanguageStore';
+
+import translations from './translations';
+
 import scss from './HematoRunRoute.module.scss';
 
 import { IconArrow } from '../../Icons/IconArrow/IconArrow';
 import { IconBgDeco } from '../../Icons/IconBgDeco/IconBgDeco';
-
 import runRoute from '../../assets/Images/hematoRunRoute/hematoRunRoute.webp';
 
 export const HematoRunRoute = () => {
+  const { language } = useLanguageStore();
+  const t = translations[language];
   return (
     <>
       <div className={scss.hematoRunRoute__header}>
-        <h2 className={scss.hematoRunRoute__header__text}>Trasa biegu</h2>
+        <h2 className={scss.hematoRunRoute__header__text}>{t.heading}</h2>
         <IconArrow />
       </div>
       <div className={scss.hematoRunRoute__background}>
