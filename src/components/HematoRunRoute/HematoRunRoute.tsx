f33b7f520510d74cd1cbda63f878187e8d8a3b7f
PiotrDrechsler <pdrech@gmail.com>
2024-03-25 12:05:41 +0100

    bacground image
    background image now is webp file
    add class with background image
    code restrucutre

--- src/components/HematoRunRoute/HematoRunRoute.tsx
+++ src/components/HematoRunRoute/HematoRunRoute.tsx
@@ -3,20 +3,21 @@ import scss from './HematoRunRoute.module.scss';
 import { IconArrow } from '../../Icons/IconArrow/IconArrow';
 
 import runRoute from '../../assets/Images/hematoRunRoute/hematoRunRoute.webp';
+import background from '../../assets/Images/hematoRunRoute/hematoRunRouteBackground.webp';
 
 export const HematoRunRoute = () => {
   return (
     <>
-      <div className={scss.hematoRunRoute}>
-        <div className={scss.hematoRunRoute__header}>
-          <h2 className={scss.hematoRunRoute__header__text}>Trasa biegu</h2>
-          <IconArrow />
-        </div>
-        <div className={scss.hematoRunRoute__container}>
+      <div className={scss.hematoRunRoute__header}>
+        <h2 className={scss.hematoRunRoute__header__text}>Trasa biegu</h2>
+        <IconArrow />
+      </div>
+      <div className={scss.hematoRunRoute__background}>
+        <div className={scss.hematoRunRoute__mapContainer}>
           <img
+            className={scss.hematoRunRoute__map}
             src={runRoute}
             about="run route"
-            className={scss.hematoRunRoute__map}
           />
         </div>
       </div>
