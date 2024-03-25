40c7a525cb9c0ecc0fdaa8008f1120c38f67181f
Astrix1234 <130665627+Astrix1234@users.noreply.github.com>
2024-03-25 13:54:54 +0000

    Deploying to gh-pages from @ Astrix1234/charity-run@a27956fbaafc68df3390af1cf6b94606fd76fd8c 🚀

---
+++ assets/LoginPage-BtCwjSWO.js
@@ -0,0 +1,1 @@
+import{j as e,H as i}from"./index-D7b6mIKt.js";function r(){return e.jsxs("div",{children:[e.jsx(i,{children:e.jsx("title",{children:"Login"})}),e.jsx("h1",{children:"Login"})]})}export{r as default};
