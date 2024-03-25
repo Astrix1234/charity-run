9deb4a4cf0f973fc0594b9b2f4ec5be606c55eea
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-25 21:19:34 +0100

    Add translation to FooterContact

---
+++ src/components/FooterContact/translations.ts
@@ -0,0 +1,18 @@
+const translations = {
+  PL: {
+    contact: 'Kontakt',
+    address: 'Adres',
+    city: '95-513 Łódź',
+    street: 'ul. Pabianicka 62',
+    mail: 'E-mail:',
+  },
+  EN: {
+    contact: 'Contact',
+    address: 'Address',
+    city: '95-513 Łódź',
+    street: '62 Pabianicka Street',
+    mail: 'E-mail:',
+  },
+};
+
+export default translations;
