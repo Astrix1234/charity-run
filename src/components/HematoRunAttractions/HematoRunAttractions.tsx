625226e85d5c82c5cbd0a82c7f24e7df3e0a5a2f
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2024-03-26 20:35:48 +0100

    translation, images, numbers

--- src/components/HematoRunAttractions/HematoRunAttractions.tsx
+++ src/components/HematoRunAttractions/HematoRunAttractions.tsx
@@ -1,29 +1,57 @@
 import React from 'react';
 import scss from './HematoRunAttractions.module.scss';
 import IdNumber from '../IdNumber/IdNumber';
-import attractions from '../../../public/images/attractions';
 import { IconBloodCell } from '../../Icons/IconBloodCell/IconBloodCell';
+import { useLanguageStore } from '../../Zustand/useLanguageStore';
+import translations from './translations';
+
+const attractions = [
+  {
+    id: 1,
+    name: '/Piknik rodzinny',
+    url: './images/attractions/Attractions-img1.jpg',
+  },
+  {
+    id: 2,
+    name: '/Spotkania ze sportowcami',
+    url: './images/attractions/Attractions-img2.jpg',
+  },
+  {
+    id: 3,
+    name: '/Scena muzyczna',
+    url: './images/attractions/Attractions-img3.jpg',
+  },
+  {
+    id: 4,
+    name: '/Strefa gastro',
+    url: './images/attractions/Attractions-img4.jpg',
+  },
+];
 
 const Attractions: React.FC = () => {
+  const { language } = useLanguageStore();
+  const t = translations[language];
+
   return (
     <section className={scss.attractions__section}>
       <div className={scss.attractions__title}>
-        <h1>Atrakcje dla uczestników</h1>
+        <h1>{t.attractionsTitle}</h1>
         <div>
           <IconBloodCell />
         </div>
       </div>
       <div className={scss.attractions__container}>
-        {attractions.map(attraction => (
+        {attractions.map((attraction, index) => (
           <div className={scss.attraction__block} key={attraction.id}>
             <img
-              src={attraction.imageUrl}
-              alt={`Atrakcja ${attraction.id}`}
+              src={attraction.url}
+              alt={`${t.attractionNames[index]}`}
               className={scss.attraction__image}
             />
             <IdNumber number={attraction.id} />
-            <div className={scss.attraction__name}>{attraction.name}</div>
-            <div className={scss.attraction__name}>{attraction.name}</div>
+            <div className={scss.attraction__name}>
+              {t.attractionNames[index]}
+            </div>
           </div>
         ))}
       </div>
