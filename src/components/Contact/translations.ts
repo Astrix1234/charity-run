7a1d654e0f8181da551342106f282d970fd2235c
Okazaki92 <vincentslominski@gmail.com>
2024-03-26 20:38:54 +0100

    feat/add contact page, SupportHand component

---
+++ src/components/Contact/translations.ts
@@ -0,0 +1,12 @@
+const translations = {
+  PL: {
+    title: 'Fundacja Na Rzecz Pomocy Chorym Na Białaczki',
+    site: 'Strona Fundacji',
+  },
+  EN: {
+    title: 'The Foundation For Leukemia Patients',
+    site: 'Foundation Site',
+  },
+};
+
+export default translations;
