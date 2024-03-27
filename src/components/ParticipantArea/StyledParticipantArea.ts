d3ab32593377598d319d0149b1de9d5e73fcd10e
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-27 15:14:23 +0100

    Add background image to ParticipantArea

--- src/components/ParticipantArea/StyledParticipantArea.ts
+++ src/components/ParticipantArea/StyledParticipantArea.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const StyledParticipant = styled.div``;
+export const StyledParticipant = styled.div`
+  background: url('/charity-run/public/images/participant-background.png')
+    no-repeat;
+  background-size: 100% 40%;
+`;
