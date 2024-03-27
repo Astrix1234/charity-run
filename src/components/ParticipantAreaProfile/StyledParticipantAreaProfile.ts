bec4abae6790228c41964de686dfd12ae36af21b
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-27 19:45:29 +0100

    Add the my profile box

--- src/components/ParticipantAreaProfile/StyledParticipantAreaProfile.ts
+++ src/components/ParticipantAreaProfile/StyledParticipantAreaProfile.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const StyledProfile = styled.div``;
+export const StyledProfile = styled.div`
+  width: 44.6875rem;
+  height: 49.625rem;
+  background-color: #ceeaeb;
+`;
