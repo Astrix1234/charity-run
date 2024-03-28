9e2833045ee43e436f2a3ee802c007a324d5830e
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2024-03-28 23:10:26 +0100

    styled the whole register page

---
+++ src/Icons/IconArrowOrange/IconArrowOrange.tsx
@@ -0,0 +1,19 @@
+export const IconArrowOrange = () => {
+  return (
+    <svg
+      width="120"
+      height="173"
+      viewBox="0 0 120 173"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M116.908 149.248C47.9684 165.967 -25.8135 61.9824 13.3231 2.0403M101.928 171.405L118.421 148.795L92.9482 134.316"
+        stroke="#FD7616"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
---
+++ src/Icons/IconStatementSquare/IconStatementSquare.tsx
@@ -0,0 +1,21 @@
+export const IconStatementSquare = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1"
+        y="1"
+        width="14"
+        height="14"
+        rx="2"
+        stroke="#1CA2A7"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
