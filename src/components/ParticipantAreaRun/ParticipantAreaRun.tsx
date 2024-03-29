d1a1018fa0324d171cf021fc8c66f2d3c2e0adb7
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-29 18:27:55 +0100

    Remove unused code from Run component

--- src/components/ParticipantAreaRun/ParticipantAreaRun.tsx
+++ src/components/ParticipantAreaRun/ParticipantAreaRun.tsx
@@ -1,9 +1,5 @@
 import { StyledRun } from './StyledParticipantAreaRun';
-import { useLanguageStore } from '../../Zustand/useLanguageStore';
-import translations from './translations';
 
 export const ParticipantAreaRun = () => {
-  const { language } = useLanguageStore();
-  const t = translations[language];
   return <StyledRun></StyledRun>;
 };
