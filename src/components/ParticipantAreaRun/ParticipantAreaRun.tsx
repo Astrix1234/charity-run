5fd62eb8dfdf9b6d8e47d0f2751750e83f1e21a8
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-29 18:29:23 +0100

    Add empty components to ParticipantAreaRun

--- src/components/ParticipantAreaRun/ParticipantAreaRun.tsx
+++ src/components/ParticipantAreaRun/ParticipantAreaRun.tsx
@@ -1,5 +1,12 @@
+import { ParticipantAreaData } from '../ParticipantAreaData/ParticipantAreaData';
+import { ParticipantAreaResults } from '../ParticipantAreaResults/ParticipantAreaRun';
 import { StyledRun } from './StyledParticipantAreaRun';
 
 export const ParticipantAreaRun = () => {
-  return <StyledRun></StyledRun>;
+  return (
+    <StyledRun>
+      <ParticipantAreaData />
+      <ParticipantAreaResults />
+    </StyledRun>
+  );
 };
