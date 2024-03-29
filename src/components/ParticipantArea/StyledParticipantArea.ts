210011416ad6f3a1cf83a5ae0e92a5a6ceeed368
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-29 19:27:46 +0100

    Change the background filename & remove unused one

--- src/components/ParticipantArea/StyledParticipantArea.ts
+++ src/components/ParticipantArea/StyledParticipantArea.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledParticipant = styled.div`
-  background: url('/charity-run/public/images/participant-background.png')
-    no-repeat;
+  background: url('/charity-run/public/images/cut-background.png') no-repeat;
   background-size: 100% 15%;
 
   .container {
