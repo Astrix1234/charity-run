670b3f471845c239d251f46cb303d56c80cf3839
PiotrDrechsler <pdrech@gmail.com>
2024-03-29 15:25:06 +0100

    add modal
    add modal react libary

--- src/components/HematoRunRoute/HematoRunRoute.tsx
+++ src/components/HematoRunRoute/HematoRunRoute.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+
 import { useLanguageStore } from '../../Zustand/useLanguageStore';
 import translations from './translations';
 import scss from './HematoRunRoute.module.scss';
@@ -6,6 +10,11 @@ import { IconBgDeco } from '../../Icons/IconBgDeco/IconBgDeco';
 import { WaveCreme } from '../../Icons/WaveCremeBackground/WaveCreme';
 
 export const HematoRunRoute = () => {
+  //modal
+  const [open, setOpen] = useState(false);
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+  //translation
   const { language } = useLanguageStore();
   const t = translations[language];
   return (
@@ -22,7 +31,15 @@ export const HematoRunRoute = () => {
           className={scss.map}
           src="images/hematoRunRoute.webp"
           about="run route"
+          onClick={onOpenModal}
         />
+        <Modal open={open} onClose={onCloseModal} center>
+          <img
+            className={scss.map__modal}
+            src="images/hematoRunRoute.webp"
+            about="run route full size"
+          />
+        </Modal>
       </div>
       <div className={scss.backgroundIcon}>
         <IconBgDeco />
