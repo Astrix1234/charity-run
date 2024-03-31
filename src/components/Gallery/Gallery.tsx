4f32d9bfcc8ea5b2e8a80a09de18e729a059bf9a
PiotrDrechsler <pdrech@gmail.com>
2024-03-31 21:47:30 +0200

    translation
    add translation to gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,16 +1,19 @@
+import { useLanguageStore } from '../../Zustand/useLanguageStore';
+
+import translations from './translations';
 import scss from './Gallery.module.scss';
 
-function Gallery() {
+const Gallery = () => {
+  const { language } = useLanguageStore();
+  const t = translations[language];
   return (
     <section className={scss.background}>
       <div className={scss.container}>
-        <h2 className={scss.heading}>
-          Galeria w trakcie przygotowania. Zdjęcia pojawią się już wkrótce.
-        </h2>
+        <h2 className={scss.heading}>{t.heading}</h2>
         <div>Gallery Container</div>
       </div>
     </section>
   );
-}
+};
 
 export default Gallery;
