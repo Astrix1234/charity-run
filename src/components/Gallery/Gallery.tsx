a7872309efd472db60b052f40662d54c0224f571
PiotrDrechsler <pdrech@gmail.com>
2024-03-31 20:18:17 +0200

    basic styles
    create styles for:
    background
    container
    heading

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -2,9 +2,14 @@ import scss from './Gallery.module.scss';
 
 function Gallery() {
   return (
-    <div className={scss.example}>
-      <h2>ehhehe</h2>
-    </div>
+    <section className={scss.background}>
+      <div className={scss.container}>
+        <h2 className={scss.heading}>
+          Galeria w trakcie przygotowania. Zdjęcia pojawią się już wkrótce.
+        </h2>
+        <div>Gallery Container</div>
+      </div>
+    </section>
   );
 }
 
