f51883ff447f5ac2302b51f156dcf3b31a6da10a
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-31 19:24:23 +0200

    Adjust font and padding in prepare section

--- src/components/ParticipantAreaPrepare/StyledParticipantAreaPrepare.ts
+++ src/components/ParticipantAreaPrepare/StyledParticipantAreaPrepare.ts
@@ -6,4 +6,19 @@ export const StyledPrepare = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   gap: 2rem;
+
+  h1,
+  p {
+    font-weight: 600;
+    padding-left: 3rem;
+    padding-right: 2rem;
+  }
+  h1 {
+    font-size: 2.5rem;
+    padding-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.25rem;
+  }
 `;
