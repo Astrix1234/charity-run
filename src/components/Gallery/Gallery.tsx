f5aeefdb9a4f21dc74da6224dbdd513726ecfa16
PiotrDrechsler <pdrech@gmail.com>
2024-03-31 19:54:09 +0200

    gallery componet
    add gallery component
    create scss style file

---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,11 @@
+import scss from './Gallery.module.scss';
+
+function Gallery() {
+  return (
+    <div className={scss.example}>
+      <h2>ehhehe</h2>
+    </div>
+  );
+}
+
+export default Gallery;
--- src/pages/GalleryPage/GalleryPage.tsx
+++ src/pages/GalleryPage/GalleryPage.tsx
@@ -1,11 +1,14 @@
 import { Helmet } from 'react-helmet-async';
 
+import Gallery from '../../components/Gallery/Gallery';
+
 export default function GalleryPage() {
   return (
     <div>
       <Helmet>
         <title>Gallery</title>
       </Helmet>
+      <Gallery />
     </div>
   );
 }
