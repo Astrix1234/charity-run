2f8df416de94eb04da686e29731885550b448541
PiotrDrechsler <pdrech@gmail.com>
2024-04-02 17:32:11 +0200

    feat/gallery
    add images to public folder
    add maping logic to component
    create styles dor img

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,9 +1,11 @@
 import { useLanguageStore } from '../../Zustand/useLanguageStore';
 
+import { images } from './Images';
+
 import translations from './translations';
 import scss from './Gallery.module.scss';
 
-const Gallery = () => {
+const Gallery: React.FC = () => {
   const { language } = useLanguageStore();
   const t = translations[language];
   return (
@@ -12,18 +14,15 @@ const Gallery = () => {
         <h2 className={scss.heading}>{t.heading}</h2>
         <div className={scss.gallery}>
           <ul className={scss.gallery__list}>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
-            <li className={scss.gallery__item}></li>
+            {images.map((image, index) => (
+              <li key={index}>
+                <img
+                  className={scss.gallery__img}
+                  src={image.src}
+                  alt={image.alt}
+                />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
