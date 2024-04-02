848f14ca3e16fb2c8e5b1a18d4cce19779453162
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2024-04-02 16:22:12 +0200

    deleted donate part and added info about costs, added icon to check passwords

--- src/components/FormRegister/translations.ts
+++ src/components/FormRegister/translations.ts
@@ -13,7 +13,16 @@ const translations = {
     button: 'Zarejestruj się',
     account: 'Masz już konto? ',
     login: 'Zaloguj się',
-    donation: 'Wesprzyj',
+    instructionsTitle:
+      'Po zarejestrowaniu się i utworzeniu konta na stronie, otrzymasz możliwość ZAPISU NA BIEG. ',
+    instructionsCost: 'Koszt zapisu na bieg wynosi:',
+    cost1: '30 zł',
+    costAdult: ' za osobę dorosłą.',
+    cost2: '20 zł',
+    costChild:
+      ' za dziecko (płatność tylko za dzieci, które będą pobierać pakiet startowy. Koszulki dziecięce będą dostępne w rozmiarach 104 i 116).',
+    costGeneral:
+      'Opłata zawiera cenę pakietu startowego, w którego skład wchodzą: koszulka z logo Fundacji, numer startowy, woda.',
   },
 
   EN: {
@@ -30,7 +39,16 @@ const translations = {
     button: 'Register',
     account: 'Already registered? ',
     login: 'Log in',
-    donation: 'Support',
+    instructionsTitle:
+      'After registering and creating an account on the website, you will receive the opportunity to SIGN UP FOR A RUN.',
+    instructionsCost: 'The cost of signing up for the run is:',
+    cost1: '30 PLN',
+    costAdult: ' per adult participant.',
+    cost2: '20 PLN',
+    costChild:
+      " per children (payment is only for children who will be receiving a starter pack. Children's shirts will be available in sizes 104 and 116).",
+    costGeneral:
+      "The fee includes the price of the starter pack, which includes: a T-shirt with the Foundation's logo, a race number, and water.",
   },
 };
 
