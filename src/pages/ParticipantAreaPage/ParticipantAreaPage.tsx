077ebd9598cd448eb0d57ced029ed304cea8e6a2
Asia <dobrowolska.krakow@gmail.com>
2024-04-03 12:08:55 +0200

    nav links corrections

--- src/pages/ParticipantAreaPage/ParticipantAreaPage.tsx
+++ src/pages/ParticipantAreaPage/ParticipantAreaPage.tsx
@@ -1,6 +1,20 @@
 import { Helmet } from 'react-helmet-async';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function ParticipantAreaPage() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const id = hash.replace('#', '');
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [hash]);
+
   return (
     <div>
       <Helmet>
