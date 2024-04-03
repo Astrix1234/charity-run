05ef264402fe469dd76de7b28b17e07a9cb95b7d
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-03 14:48:32 +0200

    Move some styling from the parent component

--- src/components/ParticipantAreaData/StyledParticipantAreaRun.ts
+++ src/components/ParticipantAreaData/StyledParticipantAreaRun.ts
@@ -2,10 +2,19 @@ import styled from 'styled-components';
 
 export const StyledData = styled.div`
   padding-right: 2rem;
+  border-right: 2px solid black;
+
   div div {
     display: flex;
     gap: 1rem;
     justify-content: right;
   }
-  border-right: 2px solid black;
+
+  p:nth-child(2n + 1) {
+    color: black;
+  }
+
+  p:nth-child(2n) {
+    font-weight: 800;
+  }
 `;
