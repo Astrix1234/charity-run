f3ee612842af1e34ba1ae4398fe121827823aa1d
PiotrDrechsler <pdrech@gmail.com>
2024-04-03 14:41:48 +0200

    pagination and rendering logic
    -create Gallery store
    -code refactor in Gallery and GalleryPagination components
    -add images to one folder
    -separete styles

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,8 +1,6 @@
 import GalleryPagination from '../GalleryPagination/GalleryPagination';
 import { useLanguageStore } from '../../Zustand/useLanguageStore';
 
-import { images } from './Images';
-
 import translations from './translations';
 import scss from './Gallery.module.scss';
 
@@ -13,21 +11,7 @@ const Gallery: React.FC = () => {
     <section className={scss.background}>
       <div className={scss.container}>
         <h2 className={scss.heading}>{t.heading}</h2>
-        <div className={scss.gallery}>
-          <GalleryPagination />
-          <ul className={scss.gallery__list}>
-            {images.map((image, index) => (
-              <li key={index}>
-                <img
-                  className={scss.gallery__img}
-                  src={image.src}
-                  alt={image.alt}
-                />
-              </li>
-            ))}
-          </ul>
-          <GalleryPagination />
-        </div>
+        <GalleryPagination />
       </div>
     </section>
   );
--- src/components/GalleryPagination/GalleryPagination.tsx
+++ src/components/GalleryPagination/GalleryPagination.tsx
@@ -1,15 +1,31 @@
 import { Pagination } from '@mui/material';
+import { useGalleryStore } from '../../Zustand/useGalleryStore';
 import scss from './GalleryPagination.module.scss';
 
 function GalleryPagination() {
+  const { page, pageCount, displayedImages, setPage } = useGalleryStore();
+
+  const handleChange = (_event: React.ChangeEvent<unknown>, page: number) => {
+    setPage(page);
+  };
+
   return (
-    <>
-      <div className={scss.container}>
-        <h1>
-          <Pagination />
-        </h1>
-      </div>
-    </>
+    <div className={scss.gallery}>
+      <h1>Current Page is {page}</h1>
+      <Pagination count={pageCount} onChange={handleChange} page={page} />
+      <ul className={scss.gallery__list}>
+        {displayedImages.map((image, index) => (
+          <li key={index}>
+            <img
+              className={scss.gallery__img}
+              src={image.src}
+              alt={image.alt}
+            />
+          </li>
+        ))}
+      </ul>
+      <Pagination count={pageCount} onChange={handleChange} page={page} />
+    </div>
   );
 }
 
