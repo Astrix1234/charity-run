4190487a2ae6bc14cf15ad5aecb3a1b6d0db9994
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-03 15:10:43 +0200

    Add my results section with styling & translations

--- src/components/ParticipantAreaResults/StyledParticipantAreaResults.ts
+++ src/components/ParticipantAreaResults/StyledParticipantAreaResults.ts
@@ -2,4 +2,18 @@ import styled from 'styled-components';
 
 export const StyledResults = styled.div`
   text-align: right;
+
+  h2,
+  h3 {
+    font-weight: 500;
+    padding-bottom: 2.5rem;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  h3 {
+    font-size: 1.75rem;
+  }
 `;
