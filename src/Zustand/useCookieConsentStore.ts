eeb40cb7698ff041265029a50df68a0edfb6e4a5
Asia <dobrowolska.krakow@gmail.com>
2024-04-04 16:50:49 +0200

    correction of logic cookies for login

--- src/Zustand/useCookieConsentStore.ts
+++ src/Zustand/useCookieConsentStore.ts
@@ -4,9 +4,14 @@ import Cookies from 'js-cookie';
 interface CookieConsentState {
   showConsent: boolean;
   setShowConsent: (show: boolean) => void;
+  resetConsent: () => void;
 }
 
 export const useCookieConsentStore = create<CookieConsentState>(set => ({
   showConsent: !Cookies.get('CookieConsent'),
   setShowConsent: show => set(() => ({ showConsent: show })),
+  resetConsent: () => {
+    Cookies.remove('CookieConsent');
+    set(() => ({ showConsent: true }));
+  },
 }));
