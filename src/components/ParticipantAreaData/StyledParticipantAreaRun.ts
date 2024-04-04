3d6af2a4922c83f99b2e34e05264e4588233ff86
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-04 19:39:40 +0200

    Build my-data section from ground up with styling

--- src/components/ParticipantAreaData/StyledParticipantAreaRun.ts
+++ src/components/ParticipantAreaData/StyledParticipantAreaRun.ts
@@ -3,12 +3,6 @@ import styled from 'styled-components';
 export const StyledData = styled.div`
   padding-right: 2rem;
 
-  div div {
-    display: flex;
-    gap: 1rem;
-    justify-content: right;
-  }
-
   p:nth-child(2n + 1) {
     color: black;
   }
--- src/components/ParticipantAreaData/translations.ts
+++ src/components/ParticipantAreaData/translations.ts
@@ -4,12 +4,14 @@ const translations = {
     nameSurname: 'Imię i nazwisko:',
     phoneNumber: 'Numer telefonu:',
     emailAddress: 'Adres e-mail:',
+    tShirt: 'Rozmiar koszulki:',
   },
   EN: {
     myData: 'My data:',
     nameSurname: 'Name and surname:',
     phoneNumber: 'Phone number:',
     emailAddress: 'E-mail address:',
+    tShirt: 'T-shirt size:',
   },
 };
 
