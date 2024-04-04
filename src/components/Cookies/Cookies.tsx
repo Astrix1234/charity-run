52e6a44934d0c75b3d1592066f9f370a0e210cc7
PiotrDrechsler <pdrech@gmail.com>
2024-04-04 14:09:30 +0200

    custom styling
    create custom styling for cookies banner

--- src/components/Cookies/Cookies.tsx
+++ src/components/Cookies/Cookies.tsx
@@ -2,7 +2,17 @@ import CookieConsent from 'react-cookie-consent';
 
 const Cookie = () => {
   return (
-    <CookieConsent enableDeclineButton overlay>
+    <CookieConsent
+      enableDeclineButton
+      overlay
+      style={{
+        backgroundColor: '#1ca2a7',
+      }}
+      buttonStyle={{
+        backgroundColor: 'hsl(25, 98%, 54%)',
+        color: 'white',
+      }}
+    >
       This website uses cookies to enhance the user experience.
     </CookieConsent>
   );
