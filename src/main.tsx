0919b0038986455c7b19266d50123ec51716b9f2
PiotrDrechsler <pdrech@gmail.com>
2024-04-04 14:38:53 +0200

    replace
    replace cookies component to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import CookiesBanner from './components/CookiesBanner/CookiesBanner.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename={'/charity-run/'}>
     <App />
-    <CookiesBanner />
   </BrowserRouter>
 );
