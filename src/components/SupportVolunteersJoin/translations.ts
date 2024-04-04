038f07f0febad6ecc0e0710b66fd2be424fd3a6b
kglanos <k.glanowski@wp.pl>
2024-04-04 21:19:29 +0200

    Create page become a Volonteers

---
+++ src/components/SupportVolunteersJoin/translations.ts
@@ -0,0 +1,12 @@
+const translations = {
+    PL: {
+        greeting: 'Dołącz do Naszego Zespołu Wolontariuszy!',
+        projectDescription: 'Szukamy serdecznych i zaangażowanych osób, które chciałyby wesprzeć naszą misję walki z białaczkami poprzez wolontariat podczas pierwszej edycji biegu charytatywnego HEMATOBIEG. Jeśli masz ochotę zaangażować się i sprawić, że to wydarzenie będzie jeszcze bardziej wyjątkowe, dołącz do naszego zespołu wolontariuszy!',
+    },
+    EN: {
+        greeting: 'Join Our Volunteers Team!',
+        projectDescription: 'We are looking for warm-hearted and committed people who would like to support our mission of fighting leukemia through volunteering during the first edition of the charity run HEMATOBIEG. If you would like to get involved and make this event even more special, join our team of volunteers!',
+    },
+};
+
+export default translations;
