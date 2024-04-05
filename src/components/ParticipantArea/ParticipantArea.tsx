b76dfd9a329137298fdc0bdbe2aca06f10756ddf
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-05 13:31:06 +0200

    Remove import of unused component

--- src/components/ParticipantArea/ParticipantArea.tsx
+++ src/components/ParticipantArea/ParticipantArea.tsx
@@ -2,7 +2,6 @@ import { StyledParticipant } from './StyledParticipantArea';
 import { ParticipantAreaHeading } from '../ParticipantAreaHeading/ParticipantAreaHeading';
 import { ParticipantAreaMain } from '../ParticipantAreaMain/ParticipantAreaMain';
 import { ParticipantAreaProfile } from '../ParticipantAreaProfile/ParticipantAreaProfile';
-import { Timetable } from '../Timetable/Timetable';
 import { ParticipantAreaPrepare } from '../ParticipantAreaPrepare/ParticipantAreaPrepare';
 
 export const ParticipantArea = () => {
