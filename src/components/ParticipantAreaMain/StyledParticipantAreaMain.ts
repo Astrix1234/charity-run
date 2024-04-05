4f6d1d64aa9570bdd9b4281745e8691bc07099f7
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-05 12:44:57 +0200

    Start styling for mobile in My profile section

--- src/components/ParticipantAreaMain/StyledParticipantAreaMain.ts
+++ src/components/ParticipantAreaMain/StyledParticipantAreaMain.ts
@@ -3,6 +3,24 @@ import styled from 'styled-components';
 export const StyledMain = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 4rem;
+  position: relative;
+
+  & > * {
+    min-width: 510px;
+    max-width: 50%;
+  }
+
+  @media screen and (max-width: 1310px) {
+    & > * {
+      min-width: 80%;
+      text-align: center;
+    }
+  }
 
   .photo {
     display: grid;
