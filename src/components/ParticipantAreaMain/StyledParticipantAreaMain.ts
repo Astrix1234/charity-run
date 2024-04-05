1e3a315140ddc9a83230c642474c2294ef54fecb
Asia <dobrowolska.krakow@gmail.com>
2024-04-05 14:29:28 +0200

    avatar

--- src/components/ParticipantAreaMain/StyledParticipantAreaMain.ts
+++ src/components/ParticipantAreaMain/StyledParticipantAreaMain.ts
@@ -1,19 +1,33 @@
 import styled from 'styled-components';
 
 export const StyledMain = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  // display: grid;
+  // grid-template-columns: repeat(2, 1fr);
 
   .photo {
-    display: grid;
-    place-content: center;
-    text-align: right;
-    gap: 1rem;
+    width: 400px;
+    height: 400px;
+    margin-bottom: 3rem;
+    // display: grid;
+    // place-content: center;
+    // text-align: right;
+    // gap: 1rem;
   }
 
-  .addPhoto {
-    display: flex;
-    justify-content: right;
-    align-items: center;
+  .imgContainer {
+    width: 100%;
+    height: 100%;
   }
+
+  .img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  // .addPhoto {
+  //   display: flex;
+  //   justify-content: right;
+  //   align-items: center;
+  // }
 `;
