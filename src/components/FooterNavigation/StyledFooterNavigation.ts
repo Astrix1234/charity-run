3010b756a804e8bcde15f1589df77fae6052c7f0
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-09 17:58:11 +0200

    Add all rights reserved at the bottom of footer

--- src/components/FooterNavigation/StyledFooterNavigation.ts
+++ src/components/FooterNavigation/StyledFooterNavigation.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
-export const StyledNavigation = styled.ul`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 14em;
+export const StyledNavigation = styled.div`
   font-size: 1.25em;
+  display: grid;
+  justify-items: center;
+  gap: 1em;
+  ul {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 12em;
+    margin-bottom: 2em;
+  }
 `;
