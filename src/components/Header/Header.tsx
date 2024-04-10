e02c81ea7fa883c3b42f63258e9eb4895189f6ea
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-04-10 07:40:50 +0200

    fixed smaller bugs for navigation, donations and attractions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
 import { useMediaQuery } from 'react-responsive';
 import DesktopHeader from '../DesktopHeader/DesktopHeader';
 import MobileHeader from '../MobileHeader/MobileHeader';
+import { useEffect } from 'react';
 
 export const Header = () => {
   const isDesktop = useMediaQuery({ query: '(min-width: 1360px)' });
+  console.log(isDesktop);
+
+  useEffect(() => {
+    if (isDesktop) document.body.style.overflow = 'auto';
+  }, [isDesktop]);
 
   if (isDesktop) return <DesktopHeader />;
 
