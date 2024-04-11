ffb7c9807e96f6aa597a3ebd82f5a1680220b217
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-11 09:24:46 +0200

    Remove foundation logotype component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import { StyledFooter } from './StyledFooter';
-import { FoundationLogotype } from '../FoundationLogotype/FoundationLogotype';
 import { FooterInformationSection } from '../FooterInformationSection/FooterInformationSection';
 import { FooterNavigation } from '../FooterNavigation/FooterNavigation';
 
