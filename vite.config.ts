5eb7eaaf109e5d6302cc06e8164605a652637cf7
Okazaki92 <vincentslominski@gmail.com>
2024-04-12 20:06:40 +0200

    fix azure connection v6

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/charity-run/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
   plugins: [react()],
 });
