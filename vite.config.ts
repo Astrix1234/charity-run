92422849c14dc44377e8f616cba840f74af8bc12
Asia <dobrowolska.krakow@gmail.com>
2024-04-12 16:27:09 +0200

    Add built assets for deployment
    :wq

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/charity-run/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   plugins: [react()],
 });
