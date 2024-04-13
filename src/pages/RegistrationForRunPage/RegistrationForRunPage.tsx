732f6d94b02ea56bcf3a38945a432a381af9a73c
Asia <dobrowolska.krakow@gmail.com>
2024-04-13 18:09:43 +0200

    navigate for logout user

--- src/pages/RegistrationForRunPage/RegistrationForRunPage.tsx
+++ src/pages/RegistrationForRunPage/RegistrationForRunPage.tsx
@@ -1,7 +1,19 @@
 import { Helmet } from 'react-helmet-async';
 import RegistrationForRun from '../../components/RegistrationForRun/RegistrationForRun';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useIsLoginStore } from '../../Zustand/useIsLoginStore';
 
 export default function RegistrationForRunPage() {
+  const navigate = useNavigate();
+  const { isLogin } = useIsLoginStore();
+
+  useEffect(() => {
+    if (!isLogin) {
+      navigate('/login');
+    }
+  }, [isLogin, navigate]);
+
   return (
     <>
       <Helmet>
