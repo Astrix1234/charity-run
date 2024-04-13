05fe2d07a3cf972c7ffc6d843b203edc307f8333
Asia <dobrowolska.krakow@gmail.com>
2024-04-13 21:05:24 +0200

    avatar

--- src/components/ParticipantAreaPhoto/ParticipantAreaPhoto.tsx
+++ src/components/ParticipantAreaPhoto/ParticipantAreaPhoto.tsx
@@ -6,7 +6,6 @@ import { userAvatar } from '../../Zustand/api';
 import { getCurrentUser } from '../../Zustand/api';
 import { useIsLoadingStore } from '../../Zustand/useIsLoadingStore';
 import { useUserDataStore } from '../../Zustand/useUserDataStore';
-import { apiUrl } from '../../Zustand/api';
 
 export const ParticipantAreaPhoto = () => {
   const { language } = useLanguageStore();
