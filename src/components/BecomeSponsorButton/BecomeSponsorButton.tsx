abb5d49eef2b48a86a5bbf079a9e7dd12d70bcdf
Asia <dobrowolska.krakow@gmail.com>
2024-04-15 14:22:06 +0200

    add function for sponsors button

--- src/components/BecomeSponsorButton/BecomeSponsorButton.tsx
+++ src/components/BecomeSponsorButton/BecomeSponsorButton.tsx
@@ -1,12 +1,18 @@
 import { useLanguageStore } from '../../Zustand/useLanguageStore';
 import { Button } from '../Button/Button';
 import translations from './translations';
+import { useNavigate } from 'react-router-dom';
 
 function BecomeSponsorButton() {
   const { language } = useLanguageStore();
   const t = translations[language];
+  const navigate = useNavigate();
 
-  return <Button content={t.text} onClick={() => {}} />;
+  const handleClick = () => {
+    navigate('/support-sponsors');
+  };
+
+  return <Button content={t.text} onClick={handleClick} />;
 }
 
 export default BecomeSponsorButton;
