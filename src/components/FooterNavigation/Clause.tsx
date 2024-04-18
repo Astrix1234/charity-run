3e6bd75327cea62551eabb70bdf492a897818acb
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-18 08:23:24 +0200

    Add modals for clause and statute

---
+++ src/components/FooterNavigation/Clause.tsx
@@ -0,0 +1,60 @@
+export const Clause = () => {
+  return (
+    <p>
+      INFORMACJA O PRZETWARZANIU DANYCH OSOBOWYCH INFORMACJA O PRZETWARZANIU
+      DANYCH OSOBOWYCH - FUNDACJA NA RZECZ POMOCY CHORYM NA BIAŁACZKI (dalej
+      “Fundacja”), we współpracy z firmami zewnętrznymi organizuje I BIEG
+      CHARYTATYWNY. Impreza odbywać się będzie stacjonarnie, w dniu 12 maja 2024
+      r. W toku imprezy będzie istniała możliwość robienia zdjęć Gości,
+      Uczestników oraz w szczególności Zwycięzców, w sposób umożliwiający ich
+      identyfikację. Zdjęcia następnie mogą zostać wykorzystane dla potrzeb
+      prowadzonych wewnętrznych i zewnętrznych działań komunikacyjnych i
+      marketingowych. Na podstawie art. 6 ust. 1 lit. a RODO, przetwarzanie
+      przez Fundację wizerunku Gości, Uczestników oraz w szczególności
+      Zwycięzców dla opisanych celów wymaga wyrażenia zgody przez osoby
+      fizyczne, których dane są przetwarzane. KLAUZULA INFORMACYJNA FUNDACJI
+      HEMATOBIEG Zgodnie z art. 13 ust. 1 i 2 Rozporządzenia Parlamentu
+      Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie
+      ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w
+      sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy
+      95/46/WE (ogólne rozporządzenie o ochronie danych), informujemy iż:
+      ADMINISTRATOR DANYCH Administratorem danych osobowych jest Fundacja Na
+      Rzecz Pomocy Chorym Na Białaczki INSPEKTOR OCHRONY DANYCH W sprawach
+      związanych z przetwarzaniem danych osobowych można kontaktować się z
+      Inspektorem Ochrony Danych pod adresem e-mail: E-mail: kontakt@fundacja.
+      hematologiczna.org CEL I PODSTAWY PRAWNE Pani/Pana dane będą przetwarzane
+      w celach: realizacji akcji Hematobiegu oraz działań podjętych w związku z
+      jej organizacją, w tym prawidłowego przeprowadzenia akcji (podstawa z art.
+      6 ust. 1 lit. b) RODO), związanych z koniecznością wypełnienia ciążącego
+      na nas obowiązku prawnego zgodnie z obowiązującymi przepisami prawa, w
+      szczególności wynikających z przepisów podatkowych, w tym wystawiania
+      zaświadczeń do celów podatkowych, prowadzenie ksiąg rachunkowych i
+      dokumentacji podatkowej (podstawa z art. 6 ust. 1 lit. c) RODO),
+      wynikających z prawnie uzasadnionych interesów Administratora, jakim jest
+      m.in. ustalenie, obrona i dochodzenie roszczeń oraz tworzenie zestawień,
+      analiz i statystyk na potrzeby wewnętrzne Administratora, dopasowanie
+      treści usług Administratora (np. serwisów internetowych) do Pani/Pana
+      potrzeby, zapewnienie bezpieczeństwa tych usług, a także ich ciągłe
+      udoskonalanie oraz rozpatrywanie zapytań i reklamacji produktów i usług
+      Administratora, marketing bezpośredni prowadzony w formie tradycyjnej (np.
+      listownej) (podstawa z art. 6 ust. 1 lit. f) RODO), wynikających z zadania
+      realizowanego w interesie publicznym art. 6ust.1 lit e) RODO, przesyłania
+      drogą elektroniczną na podany adres email informacji marketingowych, w tym
+      w formie newslettera, z najnowszymi informacjami o pracach i kampaniach,
+      ofertach oraz z wiadomościami, służących promowaniu określonych postaw,
+      zachowań oraz naszego wizerunku, a także w celu używania
+      telekomunikacyjnych urządzeń końcowych w stosunku do podanego numeru
+      telefonu dla celów marketingu bezpośredniego - w tym wysłania podziękowań
+      za wsparcie, informowania o realizacji celów statutowych organizacji,
+      informowania o organizowanych akcjach społecznych oraz możliwości dalszego
+      wsparcia organizacji oraz w celu badania prawidłowości procesu realizacji
+      darowizny i zbierania informacji zwrotnych od darczyńców (np. poprzez
+      ankietę) - w oparciu o wyrażoną dobrowolną zgodę na prowadzenie takich
+      działań (podstawa z art. 6 ust. 1 lit. a) RODO), w przypadku, gdyby
+      przetwarzanie danych osobowych miało nastąpić w innym celu niż wskazane
+      powyżej, każdorazowo odbywać się będzie po uprzednim uzyskaniu Pani/Pana
+      zgody na przetwarzanie danych osobowych, w zakresie i w celu wskazanym w
+      tej zgodzie (podstawa z art. 6 ust. 1 lit. a) RODO).
+    </p>
+  );
+};
--- src/components/FooterNavigation/FooterNavigation.tsx
+++ src/components/FooterNavigation/FooterNavigation.tsx
@@ -1,18 +1,61 @@
-import { Link } from 'react-router-dom';
+import './Modal.css';
 import { StyledNavigation } from './StyledFooterNavigation';
 import { useLanguageStore } from '../../Zustand/useLanguageStore';
 import translations from './translations';
+import { Modal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+import { useState } from 'react';
+import { Clause } from './Clause';
+import { Statute } from './Statute';
 
 export const FooterNavigation = () => {
   const { language } = useLanguageStore();
   const t = translations[language];
+  const [openFirst, setOpenFirst] = useState(false);
+  const [openSecond, setOpenSecond] = useState(false);
+  const onOpenFirstModal = () => setOpenFirst(true);
+  const onOpenSecondModal = () => setOpenSecond(true);
+  const onCloseFirstModal = () => setOpenFirst(false);
+  const onCloseSecondModal = () => setOpenSecond(false);
   return (
     <StyledNavigation>
       <ul>
         <li>
-          <Link to="#">{t.clause}</Link>
+          <p onClick={onOpenFirstModal}>{t.clause}</p>
+          <Modal
+            open={openFirst}
+            onClose={onCloseFirstModal}
+            classNames={{
+              modal: 'customModal',
+              overlay: 'customOverlay',
+            }}
+            center
+          >
+            <div className="clause">
+              <Clause />
+            </div>
+          </Modal>
+        </li>
+        <li>
+          <p className="regulamin" onClick={onOpenSecondModal}>
+            Regulamin
+          </p>
+          <Modal
+            open={openSecond}
+            onClose={onCloseSecondModal}
+            classNames={{
+              modal: 'customModal',
+              overlay: 'customOverlay',
+            }}
+            center
+          >
+            <div className="statute">
+              <Statute />
+            </div>
+          </Modal>
         </li>
       </ul>
+
       <p>
         Wszelkie prawa zastrzeżone © Fundacja Na Rzecz Pomocy Chorym Na
         Białaczki 2024
