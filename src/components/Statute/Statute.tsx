788eae22adf572266e257e8e68fbc294c1620c7a
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-18 11:03:12 +0200

    Style statute text

--- src/components/Statute/Statute.tsx
+++ src/components/Statute/Statute.tsx
@@ -3,78 +3,230 @@ import { StyledStatute } from './StyledStatute';
 export const Statute = () => {
   return (
     <StyledStatute>
-      <h1>KLAUZULA INFORMACYJNA FUNDACJI</h1>
-      <h2>HEMATOBIEG</h2>
-      <p>
-        Zgodnie z art. 13 ust. 1 i 2 Rozporządzenia Parlamentu Europejskiego i
-        Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób
-        fizycznych w związku z przetwarzaniem danych osobowych i w sprawie
-        swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE
-        (ogólne rozporządzenie o ochronie danych), informujemy iż:
+      <h1>
+        „Hematobieg”
+        <br />
+        26 maja 2024 r.
+      </h1>
+      <h2>I. ORGANIZATOR</h2>
+      <p className="end">
+        Organizatorem Biegu Charytatywnego, w ramach którego organizowane są
+        trzy biegi: „Hematobieg”, „Bieg Mam z Dziećmi”, „Bieg w Kapciach” oraz
+        spacer „Hematospacer” zwane dalej “Biegiem”, jest Fundacja Na Rzecz
+        Pomocy Chorym Na Białaczki z siedzibą w Łodzi, przy ul. Pabianickiej 62,
+        93-513 Łódź, wpisana do rejestru stowarzyszeń, innych organizacji
+        społecznych i zawodowych, fundacji oraz samodzielnych publicznych
+        zakładów opieki zdrowotnej prowadzonego przez Sąd Rejonowy dla
+        Łodzi-Śródmieścia w Łodzi pod nr KRS: 0000069531.
       </p>
-      <h2>ADMINISTRATOR DANYCH </h2>
+      <h2>II. CELE</h2>
+      <h3>Celem Biegu jest:</h3>
+      <div className="important">Pozyskanie środków:</div>
       <p>
-        Administratorem danych osobowych jest Fundacja Na Rzecz Pomocy Chorym Na
-        Białaczki
+        dla wsparcia podopiecznych Fundacji - pacjentów z chorobami
+        hematologicznymi i hematoonkologicznymi, przeznaczonych na nowoczesne
+        metody leczenia i diagnostyki chorób krwi, przeznaczonych na wspomaganie
+        edukacji i rozwoju naukowego personelu medycznego Kliniki Hematologii w
+        Łodzi w zakresie diagnostyki i leczenia chorób krwi
       </p>
-      <h2>INSPEKTOR OCHRONY DANYCH</h2>
+      <div className="important">Propagowanie wiedzy o chorobach krwi</div>
+      <div className="important">
+        Budowanie w społeczeństwie świadomości dotyczącej wyzwań stających przed
+        osobami chorymi na białaczki i ich bliskimi
+      </div>
+      <div className="important">
+        Promocja aktywności fizycznej i zdrowego stylu życia
+      </div>
+      <div className="important end">
+        Promocja działalności statutowej Fundacji Na Rzecz Pomocy Chorym Na
+        Białaczki.
+      </div>
+      <h2>III. TERMIN I MIEJSCE</h2>{' '}
+      <h3>Bieg odbędzie się 26 maja 2024 r. (niedziela) w Łodzi.</h3>{' '}
       <p>
-        W sprawach związanych z przetwarzaniem danych osobowych można
-        kontaktować się z Inspektorem Ochrony Danych pod adresem e-mail: E-mail:
-        kontakt@fundacja.hematologiczna.org
+        Dokładne trasy Biegu oraz lokalizacja Biura Zawodów zostaną podane na
+        stronie internetowej Fundacji Na Rzecz Pomocy Chorym Na Białaczki
+        www.fundacja.hematologiczna.org/news.html najpóźniej 7 dni przed dniem
+        Biegu.
       </p>
-      <h2>CEL I PODSTAWY PRAWNE</h2> Pani/Pana dane będą przetwarzane w celach:
-      <ul>
-        <li>
-          realizacji akcji Hematobiegu oraz działań podjętych w związku z jej
-          organizacją, w tym prawidłowego przeprowadzenia akcji (podstawa z art.
-          6 ust. 1 lit. b) RODO),
-        </li>
-        <li>
-          związanych z koniecznością wypełnienia ciążącego na nas obowiązku
-          prawnego zgodnie z obowiązującymi przepisami prawa, w szczególności
-          wynikających z przepisów podatkowych, w tym wystawiania zaświadczeń do
-          celów podatkowych, prowadzenie ksiąg rachunkowych i dokumentacji
-          podatkowej (podstawa z art. 6 ust. 1 lit. c) RODO),
-        </li>
-        <li>
-          wynikających z prawnie uzasadnionych interesów Administratora, jakim
-          jest m.in. ustalenie, obrona i dochodzenie roszczeń oraz tworzenie
-          zestawień, analiz i statystyk na potrzeby wewnętrzne Administratora,
-          dopasowanie treści usług Administratora (np. serwisów internetowych)
-          do Pani/Pana potrzeby, zapewnienie bezpieczeństwa tych usług, a także
-          ich ciągłe udoskonalanie oraz rozpatrywanie zapytań i reklamacji
-          produktów i usług Administratora, marketing bezpośredni prowadzony w
-          formie tradycyjnej (np. listownej) (podstawa z art. 6 ust. 1 lit. f)
-          RODO),
-        </li>
-        <li>
-          wynikających z zadania realizowanego w interesie publicznym art.
-          6ust.1 lit e) RODO,
-        </li>
-        <li>
-          przesyłania drogą elektroniczną na podany adres email informacji
-          marketingowych , w tym w formie newslettera, z najnowszymi
-          informacjami o pracach i kampaniach, ofertach oraz z wiadomościami,
-          służących promowaniu określonych postaw, zachowań oraz naszego
-          wizerunku, a także w celu używania telekomunikacyjnych urządzeń
-          końcowych w stosunku do podanego numeru telefonu dla celów marketingu
-          bezpośredniego - w tym wysłania podziękowań za wsparcie, informowania
-          o realizacji celów statutowych organizacji, informowania o
-          organizowanych akcjach społecznych oraz możliwości dalszego wsparcia
-          organizacji oraz w celu badania prawidłowości procesu realizacji
-          darowizny i zbierania informacji zwrotnych od darczyńców (np. poprzez
-          ankietę) - w oparciu o wyrażoną dobrowolną zgodę na prowadzenie takich
-          działań (podstawa z art. 6 ust. 1 lit. a) RODO),
-        </li>
-        <li>
-          w przypadku, gdyby przetwarzanie danych osobowych miało nastąpić w
-          innym celu niż wskazane powyżej, każdorazowo odbywać się będzie po
-          uprzednim uzyskaniu Pani/Pana zgody na przetwarzanie danych osobowych,
-          w zakresie i w celu wskazanym w tej zgodzie (podstawa z art. 6 ust. 1
-          lit. a) RODO).
-        </li>
-      </ul>
+      <h3>Planowana godzina startu Biegu: 11:00.</h3>
+      <p>
+        Godzina startu Biegu może zostać zmieniona. W takim przypadku
+        Organizator niezwłocznie opublikuje na stronie internetowej oraz w
+        mediach społecznościowych komunikat, wskazując godzinę rozpoczęcia
+        Biegu.
+      </p>
+      <p>
+        <b>
+          Ramowy plan dnia, w tym godziny otwarcia Biura Zawodów, zostanie
+          podany najpóźniej na 7 dni przed dniem Biegu.
+        </b>
+      </p>{' '}
+      <p>
+        Maksymalna długość trasy Biegu wynosi: 5 km, jednak Uczestnik może
+        zakończyć Bieg w dowolnym momencie, sygnalizując taką potrzebę
+        ustawionym na trasie Biegu wolontariuszom. W przypadku wcześniejszego
+        zakończenia Biegu Uczestnik powinien zgłosić się do Biura Zawodów w celu
+        odbioru pamiątkowego dyplomu.
+      </p>
+      <p>Trasy Biegu przebiegają przez tereny o jednolitej nawierzchni. </p>
+      <p>
+        Uczestnicy ustawiają się na starcie w wyznaczonym przez Organizatora
+        miejscu z przydzielonymi numerami startowymi oraz zarządzeniami
+        pomiarowymi otrzymanymi w Biurze Zawodów.
+      </p>
+      <h2>IV. ZGŁOSZENIA/ZAPISY</h2> Uczestnicy dokonują zapisów za
+      pośrednictwem formularza zgłoszeniowego dostępnego na stronie internetowej
+      Fundacji: www.fundacja.hematologiczna.org lub dedykowanej stronie biegu do
+      dnia 25 maja 2024 r. lub w odpowiednim Biurze Zawodów w dniu Biegu, tj. 26
+      maja 2024 r. w godzinach podanych w ramowym planie dnia, wypełniając i
+      podpisując kartę zgłoszeniową. Przed rozpoczęciem Biegu, każdy Uczestnik
+      podlega weryfikacji przez właściwe Biuro Zawodów. W tym celu obowiązany
+      jest posiadać ze sobą dowód osobisty lub inny dokument ze zdjęciem
+      potwierdzający tożsamość (w przypadku uczestników niepełnoletnich
+      dopuszczalna jest także legitymacja szkolna ze zdjęciem). Organizator
+      ustala limit uczestników Biegu na 450 osób. Warunkiem przyjęcia zgłoszenia
+      jest prawidłowe wypełnienie formularza zgłoszeniowego oraz uiszczenie
+      opłaty startowej w formie darowizny. Całkowity dochód z opłat startowych
+      zostanie przekazany na cele statutowe fundacji. W przypadku zgłoszenia
+      uczestnictwa za pośrednictwem formularza na stronie internetowej,
+      uczestnik zostanie przekierowany do Serwis-u Przelewy24
+      (https://www.przelewy24.pl/), celem dokonania wpłaty (darowizny na cele
+      statutowe). W dniu Biegu opłaty startowe będą zbierane przez Biuro
+      Zawodów. W miarę możliwości Organizator zapewni także możliwość wnoszenia
+      opłaty startowej za pomocą terminala płatniczego lub BLIK. Podpisanie
+      przez Uczestnika karty zgłoszeniowej do Biegu jest równoznaczne z
+      akceptacją niniejszego Regulaminu. V. WARUNKI UCZESTNICTWA Bieg ma
+      charakter otwarty. W Biegu mogą wziąć udział osoby, które najpóźniej w
+      dniu udziału w Biegu ukończą 18 lat, a także osoby niepełnoletnie, jeżeli
+      biegną razem z rodzicem lub opiekunem prawnym, za pisemną zgodą rodzica
+      lub opiekuna prawnego. W trakcie trwania Biegu wszyscy Uczestnicy
+      zobowiązani są do umieszczenia na klatce piersiowej numeru startowego
+      otrzymanego od Organizatora oraz zamocowaniu urządzenia pomiarowego.
+      Organizator w miarę możliwości udostępni przebieralnię i depozyt.
+      Informacja w tej sprawie zostanie zamieszczona na stronie internetowej
+      Fundacji Na Rzecz Pomocy Chorym Na Białaczki wraz z pozostałymi
+      informacjami organizacyjnymi dotyczącymi danej lokalizacji biegu.
+      Uczestników obowiązuje limit czasu wynoszący 1 godzinę. Po przekroczeniu
+      wskazanego czasu, Uczestnicy są zobowiązani do natychmiastowego
+      opuszczenia trasy Biegu. W przypadku konieczności skrócenia limitu czasu,
+      informacja w tej sprawie będzie dostępna w Biurze Zawodów w dniu Biegu.
+      Uczestnik, który po upływie regulaminowego czasu pozostanie na trasie,
+      czyni to na własną odpowiedzialność. Zabrania się wnoszenia na trasę biegu
+      przedmiotów, które mogą być niebezpieczne dla innych uczestników. Na
+      miejsce biegu zabrania się wnoszenia środków odurzających, dopingowych,
+      nielegalnych substancji oraz napojów alkoholowych jakiegokolwiek rodzaju.
+      Organizator zastrzega sobie prawo do odmowy dopuszczenia do udziału w
+      biegu osób, u których zostanie stwierdzone spożycie lub posiadanie
+      jakichkolwiek środków odurzających, nielegalnych substancji oraz napojów
+      alkoholowych jakiegokolwiek rodzaju albo co do których istnieje
+      uzasadnione przypuszczenie pozostawania pod wpływem takich środków. Każdy
+      z uczestników Biegu zobowiązany jest do: kulturalnego zachowania,
+      przestrzegania norm etycznych i powszechnie przyjętych zasad współżycia
+      społecznego, przestrzegania wszelkich zasad i przepisów mających
+      zastosowanie do Biegu, a w szczególności zasad Fair Play, stosowania się
+      do poleceń służb porządkowych i wolontariuszy, użytkowania dostępnych na
+      miejscu urządzeń i sprzętów w sposób prawidłowy i zgodny z ich
+      przeznaczeniem, posiadania dokumentu potwierdzającego tożsamość,
+      posiadania ubioru i obuwia sportowego, przestrzegania przepisów właściwego
+      administratora terenu. Wobec uczestników, którzy poprzez swoje zachowanie
+      w istotny sposób naruszą Regulamin zostaną wyciągnięte konsekwencje w
+      postaci: wykluczenia z Biegu, obciążenia kosztami naprawy lub wymiany
+      uszkodzonych urządzeń lub sprzętów. VI. HEMATOSPACER Z PUPILEM Na trasę
+      Biegu można wprowadzać zwierzęta tylko podczas „Hematospaceru”. W
+      pozostałych biegach obowiązuje zakaz wprowadzać zwierząt na trasę biegu.
+      Zwierzęta biorące udział w spacerze muszą być prowadzone przez uczestnika
+      na smyczy. Smycz nie powinna przekraczać długości 2 metrów. Uczestnicy,
+      których zwierzęta nie będą posiadały smyczy nie będą dopuszczeni do
+      udziału w spacerze. Każde zwierzę biorące udział w spacerze musi posiadać
+      aktualną książeczkę zdrowia wraz z aktualnymi szczepieniami. Uczestnik
+      (lub jego prawny opiekun) odpowiada za stan zdrowia oraz aktualność
+      szczepień zwierzęcia/zwierząt. Uczestnik (lub jego prawny opiekun)
+      odpowiada za zachowanie i bezpieczeństwo zwierzęcia/zwierząt podczas
+      spaceru. Uczestnik odpowiada za wszelkie ewentualne szkody wyrządzone
+      przez zwierzęta podczas trwania imprezy. Organizator nie ponosi żadnej
+      odpowiedzialności za szkody wyrządzone przez zwierzę uczestnika. Jeden
+      uczestnik może spacerować z jednym zwierzęciem. Organizator nie weryfikuje
+      umiejętności tresury/prowadzenia zwierzęcia przez zawodnika. Uczestnik
+      bierze pełną odpowiedzialność za zachowanie zwierzęcia. Organizator ma
+      prawo nie dopuścić zwierzęcia do udziału w spacerze i zażądać opuszczenia
+      miejsca spaceru w przypadku zaobserwowania zachowania, które może zagrażać
+      bezpieczeństwu pozostałych uczestników imprezy. Uczestnik biorący udział w
+      spacerze nie może w żaden sposób zmuszać zwierzęcia do biegu, ciągnąć za
+      smycz lub korzystać z pomocy osób trzecich – niezastosowanie się grozi
+      dyskwalifikacją. Zaleca się, aby zwierzę, które bierze udział w spacerze
+      miało szelki zamiast obroży. Zakazany jest start zwierzęcia w kolczatce.
+      Ze względów bezpieczeństwa, zwierzęta okazujące agresję do innych zwierząt
+      lub ludzi powinny startować w kagańcu fizjologicznym. Dla zwierząt
+      zapewniona zostanie woda. VII. KLASYFIKACJA I NAGRODY Organizator zapewnia
+      elektroniczny pomiar czasu (wraz ze zwrotnym chipem) i obsługę sędziowską.
+      Każdy Uczestnik Biegu otrzyma pamiątkowy dyplom. Spośród Uczestników,
+      którzy pokonają cały dystans Biegu zostaną wyłonieni i nagrodzeni laureaci
+      w następujących kategoriach: Bieg 5 km, podział na grupy kobiet i
+      mężczyzn: 18-35 lat 36-45 lat 46-55 lat 56-65 lat 65 + w przypadku małej
+      liczby uczestników w danej grupie, Organizator może połączyć grupy, „Bieg
+      Mam z Dziećmi”, „Bieg w Kapciach” oraz „Hematospacer” – kategoria open. Ze
+      względu na charytatywny charakter Biegu, nagrody uzależnione będą od
+      pozyskanych Sponsorów. VIII. POSTANOWIENIA KOŃCOWE Jeżeli którekolwiek z
+      postanowień Regulaminu zostanie uznane, w całości lub częściowo, za
+      nieważne lub niemożliwe do wyegzekwowania, to wszelkie inne postanowienia
+      (w całości bądź częściowo) zachowują ważność i wykonalność. Organizator
+      zastrzega sobie prawo do zmiany Regulaminu, a także zawieszenia
+      organizacji biegu z przyczyn technicznych bądź wystąpienia siły wyższej. O
+      fakcie tym poinformuje za pośrednictwem strony internetowej Fundacji oraz
+      Biura Zawodów. Organizator zapewnia Uczestnikom podstawową opiekę medyczną
+      podczas trwania Biegu. Uczestnik startuje w Biegu na własną
+      odpowiedzialność, przyjmując do wiadomości, że udział w Biegu pociąga za
+      sobą naturalne ryzyko. Organizator nie ponosi odpowiedzialności za
+      odwołanie Biegu z przyczyn od niego niezależnych i niemożliwych do
+      przewidzenia. Organizator nie ponosi odpowiedzialności za rzeczy
+      pozostawione na terenie, gdzie odbywa się Bieg. Wiążąca i ostateczna
+      interpretacja Regulaminu należy do Organizatora. Wszelkie spory w
+      pierwszej kolejności będą rozstrzygane polubownie. Sądem właściwym w
+      przypadku ewentualnego sporu pozostanie sąd właściwy miejscowo z uwagi na
+      siedzibę pozwanego. W sprawach nieuregulowanych w Regulaminie zastosowanie
+      mają powszechnie obowiązujące przepisy prawa polskiego. IX INFORMACJA O
+      PRZETWARZANIU DANYCH OSOBOWYCH Fundacja Na Rzecz Pomocy Chorym Na
+      Białaczki (dalej: „Fundacja”), we współpracy z firmami zewnętrznymi
+      organizuje Bieg Charytatywny - „Hematobieg”. Impreza odbywać się będzie
+      przed wejściem głównym do szpitala WWCOiT im. Kopernika w Łodzi, w dniu 26
+      maja 2024 r. Podczas trwania imprezy będzie istniała możliwość robienia
+      zdjęć Gości, Uczestników oraz w szczególności Zwycięzców, w sposób
+      umożliwiający ich identyfikację. Zdjęcia mogą zostać wykorzystane dla
+      potrzeb prowadzonych wewnętrznych i zewnętrznych działań komunikacyjnych i
+      marketingowych. Na podstawie art. 6 ust. 1 lit. a RODO, przetwarzanie
+      przez Fundację wizerunku Gości, Uczestników oraz w szczególności
+      Zwycięzców dla opisanych celów wymaga wyrażenia zgody przez osoby
+      fizyczne, których dane są przetwarzane. X KLAUZULA INFORMACYJNA FUNDACJI
+      Na podstawie art. 13 Rozporządzenia Parlamentu Europejskiego i Rady (UE)
+      2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w
+      związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu
+      takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o
+      ochronie danych), dalej RODO, poniżej zamieszczamy informacje dotyczące
+      przetwarzania danych osobowych w postaci wizerunku Gości, Uczestników oraz
+      w szczególności Zwycięzców podczas biegu organizowanego przez Fundację
+      dalej ADO: Administratorem Danych Osobowych jest Fundacja Na Rzecz Pomocy
+      Chorym Na Białaczki z siedzibą w Łodzi, przy ul. Pabianickiej 62, KRS
+      0000069531, e-mail: kontakt@fundacja.hematologiczna.org. ADO przetwarza
+      dane osobowe dla celów prowadzonych wewnętrznych i zewnętrznych działań
+      komunikacyjnych i marketingowych (Art. 6 ust. 1 lit. a RODO). Odbiorcami
+      danych osobowych może być firma zewnętrzna współorganizująca bieg oraz
+      inne firmy, które na zlecenie Fundacji będą przygotowywały materiały dla
+      celów komunikacyjnych i marketingowych oraz firmy ubezpieczeniowe. ADO nie
+      przekazuje danych osobowych poza Europejski Obszar Gospodarczy. Dane
+      osobowe przechowywane będą przez okres niezbędny dla celów prowadzonych
+      wewnętrznych i zewnętrznych działań komunikacyjnych i marketingowych.
+      Osoba, której dane są przetwarzane ma prawo dostępu do swoich danych oraz
+      otrzymania ich kopii, prawo do sprostowania (poprawiania) swoich danych,
+      prawo do ograniczenia przetwarzania, prawo do bycia zapomnianym, prawo do
+      sprzeciwu, prawo do przenoszenia danych, prawo do cofnięcia udzielonej
+      zgody w każdym czasie, prawo do wniesienia skargi do Urzędu Ochrony Danych
+      Osobowych. Podanie danych osobowych w zakresie objętym klauzulą jest
+      całkowicie dobrowolne. W ramach działalności ADO nie dochodzi do
+      zautomatyzowanego podejmowania decyzji, w tym profilowania, z
+      wykorzystaniem zebranych danych osobowych. Jeżeli potrzebujesz więcej
+      informacji na temat przetwarzania, udostępnienia danych zapraszamy do
+      kontaktu: kontakt@fundacja.hematologiczna.org
     </StyledStatute>
   );
 };
--- src/components/Statute/StyledStatute.ts
+++ src/components/Statute/StyledStatute.ts
@@ -3,9 +3,34 @@ import styled from 'styled-components';
 export const StyledStatute = styled.div`
   h1 {
     font-size: 1.5rem;
+    padding-bottom: min(1.5rem, 3vw);
   }
 
   h2 {
     font-size: 1.25rem;
+    padding-bottom: min(0.8rem, 2vw);
+  }
+
+  h3 {
+    font-size: 1.1rem;
+    padding-bottom: min(0.6rem, 1.5vw);
+  }
+
+  p {
+    font-size: 0.9rem;
+  }
+
+  .important {
+    font-size: 0.9rem;
+    font-weight: 700;
+    padding-top: 0.5rem;
+  }
+  .end {
+    padding-bottom: min(2rem, 4vw);
+  }
+
+  ul {
+    padding-top: min(0.5rem, 1.5vw);
+    padding-left: 0.5rem;
   }
 `;
