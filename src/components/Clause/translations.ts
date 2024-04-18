74c99f950fb63ca9330b03d83c5b2a62f36f9e7b
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-04-18 20:07:22 +0200

    Add translation to clause

---
+++ src/components/Clause/translations.ts
@@ -0,0 +1,58 @@
+const translations = {
+  PL: {
+    h1: 'KLAUZULA INFORMACYJNA FUNDACJI',
+    h2_1: 'HEMATOBIEG',
+    h2_1__p1:
+      'Zgodnie z art. 13 ust. 1 i 2 Rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych), informujemy iż:',
+    h2_2: 'ADMINISTRATOR DANYCH',
+    h2_2__p1:
+      'Administratorem danych osobowych jest Fundacja Na Rzecz Pomocy Chorym Na Białaczki',
+    h2_3: 'INSPEKTOR OCHRONY DANYCH',
+    h2_3__p1:
+      'W sprawach związanych z przetwarzaniem danych osobowych można kontaktować się z Inspektorem Ochrony Danych pod adresem e-mail:',
+    h2_3__p1_br: 'kontakt@fundacja.hematologiczna.org',
+    h2_4: 'CEL I PODSTAWY PRAWNE',
+    h2_4__p1: 'Pani/Pana dane będą przetwarzane w celach:',
+    h2_4__li1:
+      '• realizacji akcji Hematobiegu oraz działań podjętych w związku z jej organizacją, w tym prawidłowego przeprowadzenia akcji (podstawa z art. 6 ust. 1 lit. b) RODO),',
+    h2_4__li2:
+      '• związanych z koniecznością wypełnienia ciążącego na nas obowiązku prawnego zgodnie z obowiązującymi przepisami prawa, w szczególności wynikających z przepisów podatkowych, w tym wystawiania zaświadczeń do celów podatkowych, prowadzenie ksiąg rachunkowych i dokumentacji podatkowej (podstawa z art. 6 ust. 1 lit. c) RODO),',
+    h2_4__li3:
+      '• wynikających z prawnie uzasadnionych interesów Administratora, jakim jest m.in. ustalenie, obrona i dochodzenie roszczeń oraz tworzenie zestawień, analiz i statystyk na potrzeby wewnętrzne Administratora, dopasowanie treści usług Administratora (np. serwisów internetowych) do Pani/Pana potrzeby, zapewnienie bezpieczeństwa tych usług, a także ich ciągłe udoskonalanie oraz rozpatrywanie zapytań i reklamacji produktów i usług Administratora, marketing bezpośredni prowadzony w formie tradycyjnej (np. listownej) (podstawa z art. 6 ust. 1 lit. f) RODO),',
+    h2_4__li4:
+      '• wynikających z zadania realizowanego w interesie publicznym art. 6ust.1 lit e) RODO,',
+    h2_4__li5:
+      '• przesyłania drogą elektroniczną na podany adres email informacji marketingowych , w tym w formie newslettera, z najnowszymi informacjami o pracach i kampaniach, ofertach oraz z wiadomościami, służących promowaniu określonych postaw, zachowań oraz naszego wizerunku, a także w celu używania telekomunikacyjnych urządzeń końcowych w stosunku do podanego numeru telefonu dla celów marketingu bezpośredniego - w tym wysłania podziękowań za wsparcie, informowania o realizacji celów statutowych organizacji, informowania o organizowanych akcjach społecznych oraz możliwości dalszego wsparcia organizacji oraz w celu badania prawidłowości procesu realizacji darowizny i zbierania informacji zwrotnych od darczyńców (np. poprzez ankietę) - w oparciu o wyrażoną dobrowolną zgodę na prowadzenie takich działań (podstawa z art. 6 ust. 1 lit. a) RODO),',
+    h2_4__li6:
+      '• w przypadku, gdyby przetwarzanie danych osobowych miało nastąpić w innym celu niż wskazane powyżej, każdorazowo odbywać się będzie po uprzednim uzyskaniu Pani/Pana zgody na przetwarzanie danych osobowych, w zakresie i w celu wskazanym w tej zgodzie (podstawa z art. 6 ust. 1 lit. a) RODO).',
+  },
+  EN: {
+    h1: 'CLAUSE INFORMATION OF THE FOUNDATION',
+    h2_1: 'HEMATORUN',
+    h2_1__p1:
+      'According to Article 13(1) and (2) of the Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation), we inform you that:',
+    h2_2: 'DATA CONTROLLER',
+    h2_2__p1:
+      'The data controller for personal data is the Foundation for the Assistance of Leukemia Patients.',
+    h2_3: 'DATA PROTECTION OFFICER',
+    h2_3__p1:
+      'For matters related to the processing of personal data, you can contact the Data Protection Officer at the email address:',
+    h2_3__p1_br: 'kontakt@fundacja.hematologiczna.org',
+    h2_4: 'PURPOSE AND LEGAL BASIS',
+    h2_4__p1: 'Your data will be processed for the following purposes:',
+    h2_4__li1:
+      '• Carrying out the Hematobieg campaign and related activities, including proper campaign execution (based on Article 6(1)(b) of the GDPR).',
+    h2_4__li2:
+      '• Fulfilling our legal obligations, especially those arising from tax regulations, including issuing certificates for tax purposes, maintaining accounting records, and tax documentation (based on Article 6(1)© of the GDPR).',
+    h2_4__li3:
+      '• Processing based on the legally justified interests of the data controller includes activities such as determining, defending, and pursuing claims, creating reports, analyses, and statistics for internal purposes, tailoring services (e.g., websites) to your needs, ensuring the security of these services, continuous improvement, and handling inquiries and complaints related to the data controller’s products and services. Additionally, this legal basis covers traditional direct marketing (e.g., postal mail) (based on Article 6(1)(f) of the GDPR).',
+    h2_4__li4:
+      '• Processing based on the public interest task (Article 6(1)(e) of the GDPR).',
+    h2_4__li5:
+      '• Sending marketing information electronically to the provided email address, including newsletters with the latest updates on projects, campaigns, offers, and messages aimed at promoting specific attitudes, behaviors, and our image. This legal basis also covers using telecommunications end devices for direct marketing purposes, such as sending thank-you messages for support, informing about the organization’s statutory goals, notifying about social initiatives, and collecting feedback from donors (e.g., through surveys). This processing is based on voluntary consent expressed for such activities (Article 6(1)(a) of the GDPR).',
+    h2_4__li6:
+      '• If personal data processing is intended for purposes other than those mentioned above, it will always be carried out after obtaining your consent for the specific scope and purpose indicated in that consent (based on Article 6(1)(a) of the GDPR).',
+  },
+};
+
+export default translations;
