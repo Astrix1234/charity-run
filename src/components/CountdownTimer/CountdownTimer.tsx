2566ed28cc37fa2b57853624e4538e9d8f12bd94
PiotrDrechsler <pdrech@gmail.com>
2024-04-28 10:59:56 +0200

    countdown timer layout
    Create layout for component (don't write the countdown logic yet)
    
    1. html structure
    2. styles for container from figma
    3 create small font vars
    4. create colon after and before pseudo-element for middle number
    5. try to convey the look of the border from the figma

--- src/components/CountdownTimer/CountdownTimer.tsx
+++ src/components/CountdownTimer/CountdownTimer.tsx
@@ -3,7 +3,18 @@ import scss from './CountdownTimer.module.scss';
 export const CountdownTimer = () => {
   return (
     <div className={scss.countdownTimer}>
-      <div className={scss.countdownTimer__container}>COUNTDOWN TIMER</div>
+      <div className={scss.countdownTimer__container}>
+        <span className={scss.countdownTimer__text}>Dni</span>
+        <span className={scss.countdownTimer__text}>Godzin</span>
+        <span className={scss.countdownTimer__text}>Minut</span>
+        <span className={scss.countdownTimer__number}>112</span>
+        <span
+          className={`${scss.countdownTimer__semi} ${scss.countdownTimer__number}`}
+        >
+          12
+        </span>
+        <span className={scss.countdownTimer__number}>12</span>
+      </div>
     </div>
   );
 };
