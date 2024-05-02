2ef18aad6971a7b70b8a8f7d74ba7151eabc1681
PiotrDrechsler <pdrech@gmail.com>
2024-05-02 13:21:19 +0200

    implement libary to copmonent
    1. install yet-another-react-lightbox
    2. import to component
    3. add lightbox component:
      -map images in component
      -create opening for index element
      -create useState to toggle lightbox
       componnet
      -handleImageClick
      -handleLightboxClose
    4. add lazyloading to img tag  for performance optimisation

--- src/components/GalleryImages/GalleryImages.tsx
+++ src/components/GalleryImages/GalleryImages.tsx
@@ -1,34 +1,57 @@
+import { useState } from 'react';
 import { Pagination } from '@mui/material';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
 
 import { useGalleryStore } from '../../Zustand/useGalleryStore';
 import scss from './GalleryImages.module.scss';
 
 function GalleryImages() {
   const { page, pageCount, displayedImages, setPage } = useGalleryStore();
+  const [lightboxOpen, setLightboxOpen] = useState(-1);
 
-  const handleChange = (_event: React.ChangeEvent<unknown>, page: number) => {
+  const handlePageChange = (
+    _event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
     setPage(page);
+  };
+
+  const handleImageClick = (clickedIndex: number) => {
+    setLightboxOpen(clickedIndex);
+  };
+
+  const handleLightboxClose = () => {
+    setLightboxOpen(-1);
   };
 
   return (
     <div className={scss.gallery}>
       <div className={scss.gallery__pagination}>
-        <Pagination count={pageCount} onChange={handleChange} page={page} />
+        <Pagination count={pageCount} onChange={handlePageChange} page={page} />
       </div>
       <ul className={scss.gallery__list}>
-        {displayedImages.map((image, index) => (
-          <li key={index}>
+        {displayedImages.map((image, imageIndex) => (
+          <li key={imageIndex}>
             <img
               className={scss.gallery__img}
               src={image.src}
               alt={image.alt}
+              onClick={() => handleImageClick(imageIndex)}
+              loading="lazy"
             />
           </li>
         ))}
       </ul>
-      <div className={scss.gallery__pagination}>
-        <Pagination count={pageCount} onChange={handleChange} page={page} />
-      </div>
+      <Lightbox
+        index={lightboxOpen}
+        open={lightboxOpen >= 0}
+        close={handleLightboxClose}
+        slides={displayedImages.map(image => ({
+          src: image.src,
+          caption: image.alt,
+        }))}
+      />
     </div>
   );
 }
