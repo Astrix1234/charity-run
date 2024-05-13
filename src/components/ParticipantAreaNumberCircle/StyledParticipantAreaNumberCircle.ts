4591163b3e4b065bea25f3e4d83eebd1221ae3a8
Asia <dobrowolska.krakow@gmail.com>
2024-05-13 19:18:23 +0200

    header nav, footer, participant area corrections

--- src/components/ParticipantAreaNumberCircle/StyledParticipantAreaNumberCircle.ts
+++ src/components/ParticipantAreaNumberCircle/StyledParticipantAreaNumberCircle.ts
@@ -4,10 +4,10 @@ export const StyledCircle = styled.div`
   display: grid;
   place-items: center;
   color: var(--light);
-  font-family: 'DM Serif Display';
   border-radius: 50%;
   background-color: var(--orange);
   width: min(7.73rem, 25vw);
   height: min(7.73rem, 25vw);
   font-size: min(4.5rem, 50vw);
+  font-weight: 600;
 `;
