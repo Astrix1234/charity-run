92b9d0e67bbe3e652c1697ce606c8d1f8e016790
Asia <dobrowolska.krakow@gmail.com>
2024-05-14 18:00:05 +0200

    information clause correction

--- src/components/Clause/translations.ts
+++ src/components/Clause/translations.ts
@@ -1,4 +1,3 @@
-
 const translations = {
   PL: {
     h1: 'KLAUZULA INFORMACYJNA FUNDACJI',
@@ -7,8 +6,7 @@ const translations = {
       'Na podstawie art. 13 Rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych), dalej RODO, poniżej zamieszczamy informacje dotyczące przetwarzania danych osobowych Uczestników oraz w szczególności Zwycięzców podczas biegu organizowanego przez Fundację:',
     h2_2__p1:
       '1.	Administratorem Danych Osobowych (dalej: ADO) jest Fundacja Na Rzecz Pomocy Chorym Na Białaczki z siedzibą w Łodzi, przy ul. Pabianickiej 62, KRS 0000069531, e-mail: kontakt@fundacja.hematologiczna.org',
-    h2_3__p1:
-      '2.	ADO przetwarza dane osobowe dla celów:',
+    h2_3__p1: '2.	ADO przetwarza dane osobowe dla celów:',
     h2_3__li1:
       'Realizacji wydarzenia pod nazwą „Hematobieg” oraz działań podjętych w związku z jej organizacją, w tym prawidłowego przeprowadzenia akcji (podstawa z art. 6 ust. 1 lit. b) RODO),',
     h2_3__li2:
@@ -34,43 +32,41 @@ const translations = {
     h2_11__p1:
       '10. Jeżeli potrzebujesz więcej informacji na temat przetwarzania, udostępnienia danych zapraszamy do kontaktu:',
     h2_11__p1_br: 'kontakt@fundacja.hematologiczna.org ',
-  
-    },
-    EN: {
-      h1: 'CLAUSE INFORMATION OF THE FOUNDATION',
-      h2_1: 'HEMATORUN',
-      h2_1__p1:
-        'According to Article 13 of Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation), hereinafter referred to as GDPR, we provide below information regarding the processing of personal data of Participants and especially Winners during the run organized by the Foundation:',
-      h2_2__p1:
-        '1. The Data Controller (hereinafter: ADO) is the Foundation for the Assistance of Leukemia Patients, located in Łódź, at ul. Pabianicka 62, KRS 0000069531, e-mail: kontakt@fundacja.hematologiczna.org',
-      h2_3__p1:
-        '2. ADO processes personal data for the following purposes:',
-      h2_3__li1:
-        '• Carrying out the event named "Hematobieg" and activities undertaken in connection with its organization, including proper campaign execution (based on Article 6(1)(b) of the GDPR),',
-      h2_3__li2:
-        '• Fulfilling our legal obligations, especially those arising from tax regulations, including issuing certificates for tax purposes, maintaining accounting records, and tax documentation (based on Article 6(1)(c) of the GDPR),',
-      h2_3__li3:
-        '• Sending marketing information electronically to the provided email address, including newsletters with the latest updates on projects, campaigns, offers, and messages aimed at promoting specific attitudes, behaviors, and our image. This also includes using telecommunications end devices in relation to the provided phone number for direct marketing purposes - including sending thank-you messages for support, informing about the organization’s statutory goals, notifying about social initiatives, and collecting feedback from donors (e.g., through surveys) - based on voluntary consent given for such activities (based on Article 6(1)(a) of the GDPR),',
-      h2_3__li4:
-        '• Based on the legitimate interests of the data controller, such as determining, defending, and pursuing claims, creating reports, analyses, and statistics for internal purposes, tailoring services (e.g., websites) to your needs, ensuring the security of these services, continuous improvement, and handling inquiries and complaints related to the data controller’s products and services, traditional direct marketing (e.g., postal mail) (based on Article 6(1)(f) of the GDPR),',
-      h2_4__p1:
-        '3. Recipients of personal data may include an external company co-organizing the run and other companies that will prepare materials for communication and marketing purposes on behalf of the Foundation, as well as insurance companies and banks.',
-      h2_5__p1:
-        '4. ADO does not transfer personal data outside the European Economic Area.',
-      h2_6__p1:
-        '5. Personal data will be stored for the period necessary for internal and external communication and marketing activities, as well as event organization.',
-      h2_7__p1:
-        '6. The data subject has the right to access their data and receive a copy, the right to rectify (correct) their data, the right to restrict processing, the right to be forgotten, the right to object, the right to data portability, the right to withdraw consent at any time, and the right to lodge a complaint with the Data Protection Authority.',
-      h2_8__p1:
-        '7. Providing personal data within the scope covered by the clause is completely voluntary.',
-      h2_9__p1:
-        '8. In the course of ADO’s activities, no automated decision-making, including profiling, occurs using the collected personal data.',
-      h2_10__p1:
-        '9. During the event, there will be the possibility of taking photos of Guests, Participants, and especially Winners, in a way that allows for their identification. Photos may be used for internal and external communication and marketing purposes. Based on Article 6(1)(a) of the GDPR, the processing by the Foundation of the image of Guests, Participants, and especially Winners for the described purposes requires the consent of the individuals whose data is being processed.',
-      h2_11__p1:
-        '10. If you need more information about data processing and sharing, please contact us at:',
-      h2_11__p1_br: 'kontakt@fundacja.hematologiczna.org ',
-    },
+  },
+  EN: {
+    h1: 'CLAUSE INFORMATION OF THE FOUNDATION',
+    h2_1: 'HEMATORUN',
+    h2_1__p1:
+      'According to Article 13 of Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation), hereinafter referred to as GDPR, we provide below information regarding the processing of personal data of Participants and especially Winners during the run organized by the Foundation:',
+    h2_2__p1:
+      '1. The Data Controller (hereinafter: ADO) is the Foundation for the Assistance of Leukemia Patients, located in Łódź, at ul. Pabianicka 62, KRS 0000069531, e-mail: kontakt@fundacja.hematologiczna.org',
+    h2_3__p1: '2. ADO processes personal data for the following purposes:',
+    h2_3__li1:
+      'Carrying out the event named "Hematobieg" and activities undertaken in connection with its organization, including proper campaign execution (based on Article 6(1)(b) of the GDPR),',
+    h2_3__li2:
+      'Fulfilling our legal obligations, especially those arising from tax regulations, including issuing certificates for tax purposes, maintaining accounting records, and tax documentation (based on Article 6(1)(c) of the GDPR),',
+    h2_3__li3:
+      'Sending marketing information electronically to the provided email address, including newsletters with the latest updates on projects, campaigns, offers, and messages aimed at promoting specific attitudes, behaviors, and our image. This also includes using telecommunications end devices in relation to the provided phone number for direct marketing purposes - including sending thank-you messages for support, informing about the organization’s statutory goals, notifying about social initiatives, and collecting feedback from donors (e.g., through surveys) - based on voluntary consent given for such activities (based on Article 6(1)(a) of the GDPR),',
+    h2_3__li4:
+      'Based on the legitimate interests of the data controller, such as determining, defending, and pursuing claims, creating reports, analyses, and statistics for internal purposes, tailoring services (e.g., websites) to your needs, ensuring the security of these services, continuous improvement, and handling inquiries and complaints related to the data controller’s products and services, traditional direct marketing (e.g., postal mail) (based on Article 6(1)(f) of the GDPR),',
+    h2_4__p1:
+      '3. Recipients of personal data may include an external company co-organizing the run and other companies that will prepare materials for communication and marketing purposes on behalf of the Foundation, as well as insurance companies and banks.',
+    h2_5__p1:
+      '4. ADO does not transfer personal data outside the European Economic Area.',
+    h2_6__p1:
+      '5. Personal data will be stored for the period necessary for internal and external communication and marketing activities, as well as event organization.',
+    h2_7__p1:
+      '6. The data subject has the right to access their data and receive a copy, the right to rectify (correct) their data, the right to restrict processing, the right to be forgotten, the right to object, the right to data portability, the right to withdraw consent at any time, and the right to lodge a complaint with the Data Protection Authority.',
+    h2_8__p1:
+      '7. Providing personal data within the scope covered by the clause is completely voluntary.',
+    h2_9__p1:
+      '8. In the course of ADO’s activities, no automated decision-making, including profiling, occurs using the collected personal data.',
+    h2_10__p1:
+      '9. During the event, there will be the possibility of taking photos of Guests, Participants, and especially Winners, in a way that allows for their identification. Photos may be used for internal and external communication and marketing purposes. Based on Article 6(1)(a) of the GDPR, the processing by the Foundation of the image of Guests, Participants, and especially Winners for the described purposes requires the consent of the individuals whose data is being processed.',
+    h2_11__p1:
+      '10. If you need more information about data processing and sharing, please contact us at:',
+    h2_11__p1_br: 'kontakt@fundacja.hematologiczna.org ',
+  },
 };
 
 export default translations;
