4d475a055f4417e438e5f55c7f414de37081cc0a
MarekSax <mareksax@wp.pl>
2024-05-20 21:51:49 +0200

    add patrons sliders

--- src/components/HomeAll/Patrons/Patrons.tsx
+++ src/components/HomeAll/Patrons/Patrons.tsx
@@ -1,81 +1,63 @@
-import { Link } from 'react-router-dom';
 import { IconBloodCell } from '../../../Icons/IconBloodCell/IconBloodCell';
 import { useLanguageStore } from '../../../Zustand/useLanguageStore';
 import Slider from '../../Share/Slider/Slider';
 import scss from './Patrons.module.scss';
 import translations from './translations';
+import { honoraryLogos, mediaLogos, otherLogos } from './patronsLogos';
 
-const Donor = () => {
-  const { language } = useLanguageStore();
-  const t = translations[language];
-  return <h3 className={scss.marqueeFiller}>{t.becomeDonor}</h3>;
-};
-const Partner = () => {
-  const { language } = useLanguageStore();
-  const t = translations[language];
-  return <h3 className={scss.marqueeFiller}>{t.becomePartner}</h3>;
-};
+interface PatronLogo {
+  id: number;
+  logo: string;
+  link: string;
+}
 
 const Patrons = () => {
   const { language } = useLanguageStore();
   const t = translations[language];
 
-  const donorsList = [
-    <div className={scss.sponsors__logo}>
-      <Donor />
-    </div>,
-    <div className={scss.sponsors__logo}>
-      <Donor />
-    </div>,
-    <div className={scss.sponsors__logo}>
-      <Donor />
-    </div>,
-    <div className={scss.sponsors__logo}>
-      <Donor />
-    </div>,
-  ];
-  const partnersList = [
-    <div className={scss.sponsors__logo}>
-      <Partner />
-    </div>,
-    <div className={scss.sponsors__logo}>
-      <Partner />
-    </div>,
-    <div className={scss.sponsors__logo}>
-      <Partner />
-    </div>,
-    <div className={scss.sponsors__logo}>
-      <Partner />
-    </div>,
-  ];
+  const honoraryList = makePatronsList(honoraryLogos);
+
+  const mediaList = makePatronsList(mediaLogos);
+
+  const otherList = makePatronsList(otherLogos);
+
+  function makePatronsList(patronsLogos: PatronLogo[]) {
+    return patronsLogos.map(logo => (
+      <div className={scss.sponsors__logo} key={logo.id}>
+        <a href={logo.link} target="_blank">
+          <img src={logo.logo} alt="patrons logo" />
+        </a>
+      </div>
+    ));
+  }
 
   return (
     <>
       <section className={scss.padding} id="our-sponsors"></section>
-      <section className={scss.sponsors}>
-        <div className={scss.sponsors__container}>
-          <h2 className={scss.sponsors__title}>{t.patronsTitle}</h2>
-          <div className={scss.sponsors__iconBlood}>
+      <section className={scss.patrons}>
+        <div className={scss.patrons__container}>
+          <h2 className={scss.patrons__title}>{t.patronsTitle}</h2>
+          <div className={scss.patrons__iconBlood}>
             <IconBloodCell />
           </div>
         </div>
-        <div className={scss.sponsors__wrapper}>
-          <div className={scss.sponsors__subSection}>
-            <h3 className={scss.sponsors__subtitle}>{t.honorary}</h3>
-            <div className={scss.sponsors__box}>
-              <Slider array={donorsList} reverse={false} />
+        <div className={scss.patrons__wrapper}>
+          <div className={scss.patrons__subSection}>
+            <h3 className={scss.patrons__subtitle}>{t.honorary}</h3>
+            <div className={scss.patrons__box}>
+              <Slider array={honoraryList} reverse={false} />
             </div>
           </div>
-          <div className={scss.sponsors__subSection}>
-            <h3 className={scss.sponsors__subtitle}>{t.media}</h3>
-            <div className={scss.sponsors__box}>
-              <Slider array={partnersList} reverse={true} />
+          <div className={scss.patrons__subSection}>
+            <h3 className={scss.patrons__subtitle}>{t.media}</h3>
+            <div className={scss.patrons__box}>
+              <Slider array={mediaList} reverse={true} />
             </div>
           </div>
-          <div className={scss.sponsors__subSection}>
-            <h3 className={scss.sponsors__subtitle}>{t.patrons}</h3>
-            <div className={scss.sponsors__box}>
-              <Slider array={partnersList} reverse={false} />
+          <div className={scss.patrons__subSection}>
+            <h3 className={scss.patrons__subtitle}>{t.patrons}</h3>
+            <div className={scss.patrons__box}>
+              <Slider array={otherList} reverse={false} />
             </div>
           </div>
         </div>
